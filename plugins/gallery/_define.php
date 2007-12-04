5403323d0770538ca31067c000ed9aab0b517140
bruno <bruno@b35155ea-a742-4b26-8089-0891bb790021>
2007-12-04 15:41:14 +0000

    * Really flagged 0.2.8
    git-svn-id: https://svn.dotclear.net/lab@167 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/gallery/_define.php
+++ plugins/gallery/_define.php
@@ -25,7 +25,7 @@ $this->registerModule(
 	/* Name */			"Gallery",
 	/* Description*/		"Image Gallery for Dotclear2",
 	/* Author */			"Bruno Hondelatte",
-	/* Version */			'0.2.7',
+	/* Version */			'0.2.8',
 	/* Permissions */		'usage,contentadmin'
 );
 
