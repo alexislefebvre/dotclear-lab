e56da4d2a293a69071b213bafa8de37f09035d91
sacha <sacha@b35155ea-a742-4b26-8089-0891bb790021>
2008-01-25 22:05:32 +0000

    Arlequin - correction HTML
    git-svn-id: https://svn.dotclear.net/lab@305 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/arlequin/forms.php
+++ plugins/arlequin/forms.php
@@ -67,7 +67,7 @@ $mt_forms['admin_help'] = '
 	sprintf(__('A suffix to send theme setting through %sURL%s, e.g. \'%3$s\''),
 	'<acronym title="'.__('Uniform Ressource Locator').'">','</acronym>',
 	'<strong>&amp;theme=</strong>').'</td></tr>
-<tr><td>%3$s</td><td>'.__('Theme identifier').' '.'<em>à utiliser uniquement dans une URL/em>'.'</td></tr>
+<tr><td>%3$s</td><td>'.__('Theme identifier').' '.'<em>à utiliser uniquement dans une URL</em>'.'</td></tr>
 <tr><td>%4$s</td><td>'.__('Theme name').'</td></tr>
 <tr><td>%5$s</td><td>'.__('Theme description').'</td></tr>
 <tr><td>%6$s</td><td>'.__('Theme identifier').'</td></tr>
