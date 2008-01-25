7b9303b962ad91d059123230e5751e20a5ae3641
Osku <Osku@b35155ea-a742-4b26-8089-0891bb790021>
2008-01-25 22:15:18 +0000

    Initialisation du plugin dcCommentClass
    git-svn-id: https://svn.dotclear.net/lab@306 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/dcCommentClass/_define.php
@@ -0,0 +1,24 @@
+<?php /* -*- tab-width: 5; indent-tabs-mode: t; c-basic-offset: 5 -*- */
+/***************************************************************\
+ *  This is 'dcCommentClass', a plugin for Dotclear 2                *
+ *                                                             *
+ *  Copyright (c) 2007                                         *
+ *  Osku and contributors.                       *
+ *                                                             *
+ *  This is an open source software, distributed under the GNU *
+ *  General Public License (version 2) terms and  conditions.  *
+ *                                                             *
+ *  You should have received a copy of the GNU General Public  *
+ *  License along with 'Arlequin' (see COPYING.txt);           *
+ *  if not, write to the Free Software Foundation, Inc.,       *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    *
+\***************************************************************/
+
+$this->registerModule(
+	/* Name */			"Des commentaires classes",
+	/* Description*/		"Quelle classe pour mes commentaires",
+	/* Author */			"Osku",
+	/* Version */			'0.1',
+	/* Permissions */		'contentadmin'
+);
+?>
---
+++ plugins/dcCommentClass/index.php
@@ -0,0 +1,26 @@
+<?php /* -*- tab-width: 5; indent-tabs-mode: t; c-basic-offset: 5 -*- */
+/***************************************************************\
+ *  This is 'dcCommentClass', a plugin for Dotclear 2                *
+ *                                                             *
+ *  Copyright (c) 2007                                         *
+ *  Osku and contributors.                       *
+ *                                                             *
+ *  This is an open source software, distributed under the GNU *
+ *  General Public License (version 2) terms and  conditions.  *
+ *                                                             *
+ *  You should have received a copy of the GNU General Public  *
+ *  License along with 'Arlequin' (see COPYING.txt);           *
+ *  if not, write to the Free Software Foundation, Inc.,       *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    *
+\***************************************************************/
+
+
+echo '
+<html><head>
+<title>'.__('Un titre à notre page !').'</title></head><body>
+<h2>Nouveau plugin</h2>
+<p>Hello world !</p>
+</body></html>';
+
+
+?>
