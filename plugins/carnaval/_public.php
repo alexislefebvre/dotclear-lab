c4119c33e6b5bfd1fe8534f318545ded4cc56c3b
sacha <sacha@b35155ea-a742-4b26-8089-0891bb790021>
2008-01-31 17:26:25 +0000

    ajout du dossier carnaval
    git-svn-id: https://svn.dotclear.net/lab@313 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/carnaval/_public.php
@@ -0,0 +1,17 @@
+<?php /* -*- tab-width: 5; indent-tabs-mode: t; c-basic-offset: 5 -*- */
+/***************************************************************\
+ *  This is 'dcCommentClass', a plugin for Dotclear 2          *
+ *                                                             *
+ *  Copyright (c) 2007-2008                                    *
+ *  Osku and contributors.                                     *
+ *                                                             *
+ *  This is an open source software, distributed under the GNU *
+ *  General Public License (version 2) terms and  conditions.  *
+ *                                                             *
+ *  You should have received a copy of the GNU General Public  *
+ *  License along with 'dcCommentClass' (see COPYING.txt);     *
+ *  if not, write to the Free Software Foundation, Inc.,       *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    *
+\***************************************************************/
+
+?>
