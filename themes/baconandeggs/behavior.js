fb80bf524a6f82dd565d08d1a726fd264e72ac60
olivier <olivier@b35155ea-a742-4b26-8089-0891bb790021>
2008-03-03 10:40:48 +0000

    JS update on Upian-Znarf themes.
    git-svn-id: https://svn.dotclear.net/lab@16 b35155ea-a742-4b26-8089-0891bb790021

--- themes/baconandeggs/behavior.js
+++ themes/baconandeggs/behavior.js
@@ -1,62 +1,11 @@
-function $()
-{
-  var elements = new Array();
-
-  for (var i = 0; i < arguments.length; i++) {
-    var element = arguments[i];
-    if (typeof element == 'string')
-      element = document.getElementById(element);
-
-    if (arguments.length == 1)
-      return element;
-
-    elements.push(element);
-  }
-
-  return elements;
-}
-
-Object.extend = function(destination, source) {
-  for (property in source) {
-    destination[property] = source[property];
-  }
-  return destination;
-}
-
-if (!window.Element) {
-  var Element = new Object();
-}
-
-Object.extend(Element, {
-  getHeight: function(element) {
-    element = $(element);
-    return element.offsetHeight;
-  }
+$(function() {
+	$('#wrapper').css('height','auto');
+	
+	var occupedHeight = $('#top').height() + $('#footer').height();
+	var newMainHeight = $('body').height() - occupedHeight;
+	
+	if(newMainHeight < $('#wrapper').height()) {
+		newMainHeight = $('#wrapper').height();
+	}
+	$('#wrapper').css('height',newMainHeight + 'px');
 });
-
-function getTotalHeight()
-{
-	if (self.innerHeight) {
-		return parseInt(self.innerHeight);
-	} else if (document.documentElement && document.documentElement.clientHeight) {
-		return document.documentElement.clientHeight;
-	} else if (document.body) {
-		return document.body.clientHeight;
-	}
-}
-
-init = function()
-{
-
-  $('wrapper').style.height = 'auto';
-  var occupedHeight = Element.getHeight('top') + Element.getHeight('footer');
-  var newMainHeight = getTotalHeight() - occupedHeight;
-  if(newMainHeight < Element.getHeight('wrapper')) {
-		newMainHeight = Element.getHeight('wrapper');
-	}
-	$('wrapper').style.height = newMainHeight + 'px';
-
-}
-
-window.onload = init
-window.onresize = init
