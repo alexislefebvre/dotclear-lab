4c848785bd666938a8363d1cbe167a3ecd45fa1a
sacha <sacha@b35155ea-a742-4b26-8089-0891bb790021>
2008-04-20 10:16:26 +0000

    Jabber Notifications:  * Adding maing.lang.php  * Setting svn:ignore to test.php
    git-svn-id: https://svn.dotclear.net/lab@413 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/jabberNotifications/locales/fr/main.lang.php
@@ -0,0 +1,81 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+# This file is part of DotClear.
+# Copyright (c) 2005 Olivier Meunier. All rights
+# reserved.
+#
+# DotClear is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# DotClear is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with DotClear; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# ***** END LICENSE BLOCK *****#
+#
+#
+#        DOT NOT MODIFY THIS FILE !
+
+
+
+
+$GLOBALS['__l10n']['Jabber notifications'] = 'Notifications Jabber';
+$GLOBALS['__l10n']['Jabber ID is empty'] = 'L\'identifiant Jabber est vide';
+$GLOBALS['__l10n']['Invalid Jabber ID'] = 'Identifiant Jabber invalide';
+$GLOBALS['__l10n']['Invalid notification status'] = 'Statut de notification invalide';
+$GLOBALS['__l10n']['Entry: %s <%s>'] = 'Billet : %s <%s>';
+$GLOBALS['__l10n']['Comment by: %s <%s>'] = 'Commentaire par : %s <%s>';
+$GLOBALS['__l10n']['Website: %s'] = 'Site web : %s';
+$GLOBALS['__l10n']['You received a new comment on the blog \'%s\':'] = 'Vous avez reçu un nouveau commentaire sur le blog "%s"';
+$GLOBALS['__l10n']['You received a new trackback on the blog \'%s\':'] = 'Vous avez reçu un nouveau rétrolien sur le blog "%s"';
+$GLOBALS['__l10n']['never'] = 'jamais';
+$GLOBALS['__l10n']['for my entries'] = 'pour mes billets';
+$GLOBALS['__l10n']['for my default blog'] = 'pour mon blog par défaut';
+$GLOBALS['__l10n']['for all my blogs'] = 'pour tous mes blogs';
+$GLOBALS['__l10n']['all blogs'] = 'pour tous les blogs';
+$GLOBALS['__l10n']['Jabber account setup'] = 'Paramètres du compte Jabber';
+$GLOBALS['__l10n']['Enable Jabber notifications'] = 'Activer les notifications Jabber';
+$GLOBALS['__l10n']['Username:'] = 'Nom d\'utilisateur :';
+$GLOBALS['__l10n']['Password:'] = 'Mot de passe :';
+$GLOBALS['__l10n']['Server:'] = 'Serveur :';
+$GLOBALS['__l10n']['Port:'] = 'Port :';
+$GLOBALS['__l10n']['HTTP gateway (leave empty to disable):'] = 'Passerelle HTTP (laissez vide pour désactiver) :';
+$GLOBALS['__l10n']['save'] = 'enregistrer';
+$GLOBALS['__l10n']['Setup test'] = 'Test des paramètres';
+$GLOBALS['__l10n']['Destination JabberID:'] = 'JabberID de destination :';
+$GLOBALS['__l10n']['test'] = 'tester';
+$GLOBALS['__l10n']['Notify new comments using Jabber:'] = 'Avertir de nouveaux commentaires par Jabber :';
+$GLOBALS['__l10n']['JabberID that should be notified:'] = 'Identifiant Jabber à avertir :';
+$GLOBALS['__l10n']['Jabber notifications have been disabled.'] = 'Les notifications Jabber ont été désactivées';
+$GLOBALS['__l10n']['Username was not set'] = 'Nom d\'utilisateur non spécifié';
+$GLOBALS['__l10n']['Password was not set'] = 'Mot de passe non spécifié';
+$GLOBALS['__l10n']['Server was not set'] = 'Serveur non spécifié';
+$GLOBALS['__l10n']['Port was not set (default is %d)'] = 'Numéro de port non spécifié (le port par défaut est %d)';
+$GLOBALS['__l10n']['Port must be a number, e.g. %d'] = 'Le numéro de port doit être un nombre, p.ex. %d';
+$GLOBALS['__l10n']['Settings have been successfully updated.'] = 'Paramètres enregistrés avec succès.';
+$GLOBALS['__l10n']['The following fields contain errors :'] = 'Les champs suivants contiennent des erreurs :';
+$GLOBALS['__l10n']['Unexpected error at line %d in %s'] = 'Erreur inattendue à la ligne %d du fichier %s';
+$GLOBALS['__l10n']['Settings have been initialized.'] = 'Les paramètres ont été initialisés.';
+$GLOBALS['__l10n']['Unknown status:'] = 'Statut inconnu :';
+$GLOBALS['__l10n']['Test result (in %.3f s):'] = 'Résultat du test (en %.3f s):';
+$GLOBALS['__l10n']['Configuration'] = 'Configuration';
+$GLOBALS['__l10n']['Help'] = 'Aide';
+$GLOBALS['__l10n']['Message succefully sended.'] = 'Message envoyé avec succès.';
+$GLOBALS['__l10n']['I did not finished. It can mean connection timeout.'] = 'Je n\'ai pas fini. Cela peut signifier l\'expiration de la connexion.';
+$GLOBALS['__l10n']['Connection to the gateway failed.'] = 'La connexion à la passerelle a échoué.';
+$GLOBALS['__l10n']['Connection to the jabber server failed.'] = 'La connexion au serveur Jabber a échoué.';
+$GLOBALS['__l10n']['Login failure.'] = 'Erreur lors de l\'identification.';
+$GLOBALS['__l10n']['Authentification failure.'] = 'Erreur d\'authentification.';
+$GLOBALS['__l10n']['Unable to send message.'] = 'Impossible d\'envoyer le message.';
+$GLOBALS['__l10n']['A stream error occured during connection.'] = 'Une erreur de flux s\'est produite pendant la connexion.';
+$GLOBALS['__l10n']['You can test this configuration without saving changes.'] = 'Vous pouvez tester cette configuration sans enregistrer les changements.';
+$GLOBALS['__l10n']['Secure connection:'] = 'Connexion sécurisée :';
+$GLOBALS['__l10n']['None'] = 'Aucune';
+?>
