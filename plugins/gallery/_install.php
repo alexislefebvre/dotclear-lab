ef0f19cfa245efaf3d66d2bdbf96b61660a0c888
bruno <bruno@b35155ea-a742-4b26-8089-0891bb790021>
2008-11-03 19:02:30 +0000

    Far better handled this way...
    git-svn-id: https://svn.dotclear.net/lab@258 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/gallery/_install.php
+++ plugins/gallery/_install.php
@@ -14,6 +14,10 @@ global $core;
 
 $this_version = $core->plugins->moduleInfo('gallery','version');
 $installed_version = $core->getVersion('gallery');
+
+if (version_compare($installed_version,$this_version,'>=')) {
+	return;
+}
  
 function putGlobalSetting($id,$value,$type=null,$label=null,$value_change=true) {
 	global $core;
@@ -23,23 +27,19 @@ function putGlobalSetting($id,$value,$type=null,$label=null,$value_change=true) 
 	else
 		$core->blog->settings->put($id,$old_value,$type,$label,$value_change,true);
 }
-if (version_compare($installed_version,$this_version,'!=')) {
-	$core->blog->settings->setNamespace('gallery');
-	putGlobalSetting('gallery_galleries_url_prefix','galleries','string','Gallery lists URL prefix');
-	putGlobalSetting('gallery_gallery_url_prefix','gallery','string','Galleries URL prefix');
-	putGlobalSetting('gallery_image_url_prefix','image','string','Images URL prefix');
-	putGlobalSetting('gallery_default_theme','default','string','Default theme to use');
-	putGlobalSetting('gallery_nb_images_per_page',24,'integer','Number of images per page');
-	putGlobalSetting('gallery_nb_galleries_per_page',10,'integer','Number of galleries per page');
-	putGlobalSetting('gallery_new_items_default','YYYYN','string','Default options for new items management');
-	putGlobalSetting('gallery_galleries_sort','date','string','Galleries list sort criteria');
-	putGlobalSetting('gallery_galleries_order','DESC','string','Galleries list sort order criteria');
-	putGlobalSetting('gallery_galleries_orderbycat',true,'boolean','Galleries list group by category');
-	putGlobalSetting('gallery_enabled',false,'boolean','Gallery plugin enabled');
-	putGlobalSetting('gallery_enabled',false,'boolean','Gallery plugin enabled');
-	putGlobalSetting('gallery_themes_path','plugins/gallery/default-templates','string','Gallery Themes path');
-	return;
-}
+$core->blog->settings->setNamespace('gallery');
+putGlobalSetting('gallery_galleries_url_prefix','galleries','string','Gallery lists URL prefix');
+putGlobalSetting('gallery_gallery_url_prefix','gallery','string','Galleries URL prefix');
+putGlobalSetting('gallery_image_url_prefix','image','string','Images URL prefix');
+putGlobalSetting('gallery_default_theme','default','string','Default theme to use');
+putGlobalSetting('gallery_nb_images_per_page',24,'integer','Number of images per page');
+putGlobalSetting('gallery_nb_galleries_per_page',10,'integer','Number of galleries per page');
+putGlobalSetting('gallery_new_items_default','YYYYN','string','Default options for new items management');
+putGlobalSetting('gallery_galleries_sort','date','string','Galleries list sort criteria');
+putGlobalSetting('gallery_galleries_order','DESC','string','Galleries list sort order criteria');
+putGlobalSetting('gallery_galleries_orderbycat',true,'boolean','Galleries list group by category');
+putGlobalSetting('gallery_enabled',false,'boolean','Gallery plugin enabled');
+putGlobalSetting('gallery_themes_path','plugins/gallery/default-templates','string','Gallery Themes path');
 
 $core->setVersion('gallery',$this_version);
 
