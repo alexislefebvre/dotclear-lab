8db6e0b2200bd0f9020d51ca92be42c74dcef2f0
vincent <vincent@b35155ea-a742-4b26-8089-0891bb790021>
2008-12-09 13:51:55 +0000

    tagging blogrollpage 0.1
    git-svn-id: https://svn.dotclear.net/lab@592 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/blogrollpage/tags/0.1/_define.php
@@ -0,0 +1,19 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# This file is part of blogrollpage, a plugin for DotClear2.
+#
+# Copyright (c) 2008 Vincent Garnier and contributors
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */				"Blogroll Page",
+	/* Description*/		"Display blogroll on a page",
+	/* Author */			"Vincent Garnier",
+	/* Version */			'0.1'
+);
