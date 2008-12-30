08941c2fabf5d28850108d5022061078286f42e2
sacha <sacha@b35155ea-a742-4b26-8089-0891bb790021>
2008-12-30 22:51:25 +0000

    removed showip plugin
    git-svn-id: https://svn.dotclear.net/lab@615 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/showip/_define.php
+++
@@ -1,7 +0,0 @@
-<?php
-# This file is in public domain
-
-if (!defined('DC_RC_PATH')) { return; }
-
-$this->registerModule('showip','Shows visitors IP address','Oleksandr Syenchuk','0.1','usage');
-?>
