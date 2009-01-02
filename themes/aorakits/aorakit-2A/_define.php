d8503ba64b40af5e817d7f5961b5c274b9e65888
kozlika <kozlika@b35155ea-a742-4b26-8089-0891bb790021>
2009-01-02 10:19:52 +0000

    Rectif url des aorakits dans les blocs de licence
    git-svn-id: https://svn.dotclear.net/lab@640 b35155ea-a742-4b26-8089-0891bb790021

--- themes/aorakits/aorakit-2A/_define.php
+++ themes/aorakits/aorakit-2A/_define.php
@@ -1,7 +1,7 @@
 <?php
 # ***** BEGIN LICENSE BLOCK *****
 #
-# Copyright (c) 2007-2009 Kozlika and contributors
+# Copyright (c) 2006-2009 Kozlika and contributors
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
