76763bea0356bf1738b542499fdfff428b7c1ade
nicofrand <nicofrand@b35155ea-a742-4b26-8089-0891bb790021>
2009-01-02 23:48:30 +0000

    Adding error :/
    git-svn-id: https://svn.dotclear.net/lab@643 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/tweakurls/feed2img/_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('DC_CONTEXT_ADMIN')) { return; }
-require_once(dirname(__FILE__).'/inc/feed2img.class.php');
-$core->addBehavior('adminAfterPostCreate', array('EntriesSelection','ImgOutput'));
-$core->addBehavior('adminAfterPostUpdate', array('EntriesSelection','ImgOutput'));
-$core->addBehavior('adminAfterPostDelete', array('EntriesSelection','ImgOutput'));
-?>
