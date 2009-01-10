109926cc3051a4527c2e9bcc051be40f54dc6ec5
kwon <kwon@b35155ea-a742-4b26-8089-0891bb790021>
2009-01-10 20:27:13 +0000

    version 0.1.3 : correction du template comListe.html
    git-svn-id: https://svn.dotclear.net/lab@688 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/comListe/_define.php
+++ plugins/comListe/_define.php
@@ -27,7 +27,7 @@ $this->registerModule(
         /* Name */                      "ComListe",
         /* Description*/                "Plugin for printing comments list",
         /* Author */                    "Benoit de Marne",
-        /* Version */                   '0.1.2',
+        /* Version */                   '0.1.3',
         /* Permissions */               'admin'
 );
 
