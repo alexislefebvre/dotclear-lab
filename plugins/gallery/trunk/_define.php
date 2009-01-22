cfae3ef7dcb9c91c135571902a3238c22e46d1de
bruno <bruno@b35155ea-a742-4b26-8089-0891bb790021>
2009-01-22 09:01:47 +0000

    0.3.2
    git-svn-id: https://svn.dotclear.net/lab@719 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/gallery/trunk/_define.php
+++ plugins/gallery/trunk/_define.php
@@ -15,7 +15,7 @@ $this->registerModule(
 	/* Name */			"Gallery",
 	/* Description*/		"Image Gallery for Dotclear2",
 	/* Author */			"Bruno Hondelatte",
-	/* Version */			'0.3.1',
+	/* Version */			'0.3.2',
 	/* Permissions */		'usage,contentadmin'
 );
 
