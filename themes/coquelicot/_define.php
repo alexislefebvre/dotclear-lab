354b7b2fca7b7bc69ea44fd3372bd2f5d37bc59e
RenardBlanc <RenardBlanc@b35155ea-a742-4b26-8089-0891bb790021>
2009-02-07 14:26:20 +0000

    Version 1.0 version final, instruction en cours de frappe.
    git-svn-id: https://svn.dotclear.net/lab@747 b35155ea-a742-4b26-8089-0891bb790021

--- themes/coquelicot/_define.php
+++ themes/coquelicot/_define.php
@@ -1,19 +1,28 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
+# ***** BEGIN LICENSE BLOCK *****
+# This file is part of DotClear.
+# Copyright (c) 2005 Marc Hertzog and contributors. All rights
+# reserved.
 #
-# This file is part of Graylvetica, a Dotclear 2 theme.
+# DotClear is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# Copyright (c) 2003-2008 Olivier Meunier and contributors
-# Licensed under the GPL version 2.0 license.
-# See LICENSE file or
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# DotClear is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# -- END LICENSE BLOCK ------------------------------------
-if (!defined('DC_RC_PATH')) { return; }
+# You should have received a copy of the GNU General Public License
+# along with DotClear; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# ***** END LICENSE BLOCK *****
 
 $this->registerModule(
 	/* Name */				"Coquelicot",
-	/* Description*/		"Sur un Kit base 3 colonnes fixe, nav-main-extra",
+	/* Description*/		"Kit Aorakits 3 colonnes fixe, nav-main-extra",
 	/* Author */			"RenardBlanc",
 	/* Version */			'1.0'
 );
