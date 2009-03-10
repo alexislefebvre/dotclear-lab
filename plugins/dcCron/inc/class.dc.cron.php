449b1d0cfd20d670bc7f0e20f04d1465ff8e036c
Tomtom33 <Tomtom33@b35155ea-a742-4b26-8089-0891bb790021>
2009-03-10 07:09:34 +0000

    Fixed new bugs :p
    git-svn-id: https://svn.dotclear.net/lab@855 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/dcCron/inc/class.dc.cron.php
+++ plugins/dcCron/inc/class.dc.cron.php
@@ -263,7 +263,7 @@ class dcCron
 	 */
 	public function getTaskInterval($nid)
 	{
-		return array_key_exists($nid,$this->tasks) ? $this->task[$nid]['interval'] : false;
+		return array_key_exists($nid,$this->tasks) ? $this->tasks[$nid]['interval'] : false;
 	}
 
 	/**
@@ -275,7 +275,7 @@ class dcCron
 	 */
 	public function getNextRunDate($nid)
 	{
-		return array_key_exists($nid,$this->tasks) ? $this->task[$nid]['last_run'] + $this->task[$nid]['interval'] : false;
+		return array_key_exists($nid,$this->tasks) ? $this->tasks[$nid]['last_run'] + $this->tasks[$nid]['interval'] : false;
 	}
 
 	/**
