d2a8280920f1a40ec689de363eb364ce45d62fe4
kwon <kwon@b35155ea-a742-4b26-8089-0891bb790021>
2009-03-17 21:18:43 +0000

    Newsletter 3.5.5rc1 - change license - fix captcha - change the file structure
    git-svn-id: https://svn.dotclear.net/lab@898 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/newsletter/trunk/inc/db-schema.php
+++ plugins/newsletter/trunk/inc/db-schema.php
@@ -1,24 +1,14 @@
 <?php
-# ***** BEGIN LICENSE BLOCK *****
-# This file is part of Newsletter, a plugin for Dotclear 2.
-# Copyright (C) 2009 Benoit de Marne, and contributors. All rights
-# reserved.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-# ***** END LICENSE BLOCK *****
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of Newsletter, a plugin for Dotclear.
+# 
+# Copyright (c) 2009 Benoit de Marne
+# benoit.de.marne@gmail.com
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
 
 if (!($_s instanceof dbStruct)) { 
 	throw new Exception('No valid schema object'); 
