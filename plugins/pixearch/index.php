e1237e8db9a96b4827c050cc49ec630fafe440b7
hadrien <hadrien@b35155ea-a742-4b26-8089-0891bb790021>
2009-03-18 07:22:50 +0000

    S'il existe UN truc chiant dans le dev, c'est bien la licence d'utilisation. Ca devait etre mieux avant...
    git-svn-id: https://svn.dotclear.net/lab@901 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/pixearch/index.php
+++ plugins/pixearch/index.php
@@ -1,27 +1,27 @@
 <?php
 # ***** BEGIN LICENSE BLOCK *****
-# This file is part of DotClear.
-# Copyright (c) 2007 Olivier Meunier and contributors.
+# This file is part of Pixearch.
+# Copyright (c) 2009 Hadrien Lanneau.
 # All rights reserved.
 #
-# DotClear is free software; you can redistribute it and/or modify
+# Pixearch is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 # 
-# DotClear is distributed in the hope that it will be useful,
+# Pixearch is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with DotClear; if not, write to the Free Software
+# along with Pixearch; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# This file is part of Pixearch
-# Hadrien Lanneau http://www.alti.info/pages/pixearch
+# http://www.alti.info/pages/pixearch
 #
-# ***** END LICENSE BLOCK *****global $core;
+# ***** END LICENSE BLOCK *****
+global $core;
 
 include_once dirname(__FILE__).'/inc/Pixearch.class.php';
 
