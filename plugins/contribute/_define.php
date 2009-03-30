c92401bfd71f1606178016d8bee517b975f05e55
Moe <Moe@b35155ea-a742-4b26-8089-0891bb790021>
2009-03-30 19:35:43 +0000

    '''Contribute 1.0-alpha15''' :  * corrected wrong text strings (thanks again to brol)
    git-svn-id: https://svn.dotclear.net/lab@967 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/contribute/_define.php
+++ plugins/contribute/_define.php
@@ -27,7 +27,7 @@ $this->registerModule(
      /* Name */                      "Contribute",
      /* Description*/                "Allow visitors to contribute to your blog",
      /* Author */                    "Moe (http://gniark.net/)",
-     /* Version */                   '1.0-alpha14',
+     /* Version */                   '1.0-alpha15',
      /* Permissions */               'admin'
 );
 ?>
