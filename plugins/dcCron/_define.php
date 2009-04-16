811c9272ae0111864a560205258e50a9a68dd463
Tomtom33 <Tomtom33@b35155ea-a742-4b26-8089-0891bb790021>
2009-04-16 12:38:48 +0000

    dcCron 1.1.5 :  * I do respect my rules :p
    git-svn-id: https://svn.dotclear.net/lab@1031 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/dcCron/_define.php
+++ plugins/dcCron/_define.php
@@ -16,7 +16,7 @@ $this->registerModule(
 		/* Name */			"dcCron",
 		/* Description*/		"Schedule any tasks",
 		/* Author */			"Tomtom (http://blog.zenstyle.fr)",
-		/* Version */			'1.1.4',
+		/* Version */			'1.1.5',
 		/* Permissions */		'admin',
 							null,
 		/* Priority */			10000
