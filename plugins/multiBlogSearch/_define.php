32dcab7f410bee860dc6bb832d3af265e78d58f3
Tomtom33 <Tomtom33@b35155ea-a742-4b26-8089-0891bb790021>
2009-04-21 21:07:17 +0000

    multiBlogSearch 0.3.4 :  * Fixed licence blocks  * Added support <tpl:Widget> tag
    git-svn-id: https://svn.dotclear.net/lab@1055 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/multiBlogSearch/_define.php
+++ plugins/multiBlogSearch/_define.php
@@ -3,7 +3,7 @@
 # This file is part of multiBlogSearch, a plugin for Dotclear.
 # 
 # Copyright (c) 2009 Tomtom
-# http://blog.zesntyle.fr/
+# http://blog.zenstyle.fr/
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
@@ -14,7 +14,7 @@ $this->registerModule(
 		/* Name */			"MultiBlogSearch",
 		/* Description*/		"Find all posts in all blogs",
 		/* Author */			"Tomtom (http://blog.zenstyle.fr)",
-		/* Version */			'0.3.3',
+		/* Version */			'0.3.4',
 		/* Permissions */		'usage,contentadmin'
 );
 
--- plugins/multiBlogSearch/_prepend.php
+++ plugins/multiBlogSearch/_prepend.php
@@ -3,7 +3,7 @@
 # This file is part of multiBlogSearch, a plugin for Dotclear.
 # 
 # Copyright (c) 2009 Tomtom
-# http://blog.zesntyle.fr/
+# http://blog.zenstyle.fr/
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
@@ -11,5 +11,7 @@
 # -- END LICENSE BLOCK ------------------------------------
 
 $__autoload['multiBlogSearch'] = dirname(__FILE__).'/inc/class.multi.blog.search.php';
+
+require dirname(__FILE__).'/_widgets.php';
 
 ?>
