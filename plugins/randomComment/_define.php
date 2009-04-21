1a80c25d3fdc083f2d2fd350aa993b970b342241
Tomtom33 <Tomtom33@b35155ea-a742-4b26-8089-0891bb790021>
2009-04-21 21:14:37 +0000

    randomComment 1.0.3 :  * Fixed licence blocks  * Added support <tpl:Widget> tag
    git-svn-id: https://svn.dotclear.net/lab@1058 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/randomComment/_define.php
+++ plugins/randomComment/_define.php
@@ -3,7 +3,7 @@
 # This file is part of randomComment, a plugin for Dotclear.
 # 
 # Copyright (c) 2009 Tomtom
-# http://blog.zesntyle.fr/
+# http://blog.zenstyle.fr/
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
@@ -16,7 +16,7 @@ $this->registerModule(
 		/* Name */			'randomComment',
 		/* Description */		'Display random comments on your blog',
 		/* Author */			'Tomtom (http://plugins.zenstyle.fr/)',
-		/* Version */			'1.0.2',
+		/* Version */			'1.0.3',
 		/* Premission */		'usage,contentadmin'
 );
 
--- plugins/randomComment/_prepend.php
+++ plugins/randomComment/_prepend.php
@@ -3,7 +3,7 @@
 # This file is part of randomComment, a plugin for Dotclear.
 # 
 # Copyright (c) 2009 Tomtom
-# http://blog.zesntyle.fr/
+# http://blog.zenstyle.fr/
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
@@ -13,5 +13,7 @@
 $__autoload['randomComment'] = dirname(__FILE__).'/inc/class.random.comment.php';
 
 $core->url->register('randomComment','randomComment','^randomComment$',array('randomCommentUrl','randomComment'));
+
+require dirname(__FILE__).'/_widgets.php';
 
 ?>
