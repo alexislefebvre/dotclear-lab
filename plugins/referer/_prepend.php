9588873456e9ca5312e7c73769f01084a61fa998
Tomtom33 <Tomtom33@b35155ea-a742-4b26-8089-0891bb790021>
2009-04-21 21:17:15 +0000

    referer 0.3.1 :  * Fixed licence blocks  * Added support <tpl:Widget> tag
    git-svn-id: https://svn.dotclear.net/lab@1059 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/referer/_prepend.php
+++ plugins/referer/_prepend.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of referer, a plugin for Dotclear.
 # 
@@ -10,14 +10,6 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # -- END LICENSE BLOCK ------------------------------------
 
-if (!defined('DC_RC_PATH')) { return; }
-
-$this->registerModule(
-		/* Name */			'referer',
-		/* Description */		'Displays your blog referers ',
-		/* Author */			'Tomtom (http://plugins.zenstyle.fr/)',
-		/* Version */			'0.3.1',
-		/* Permissions */		'usage,contentadmin'
-);
+require dirname(__FILE__).'/_widgets.php';
 
 ?>
