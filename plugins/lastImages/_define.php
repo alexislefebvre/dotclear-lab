88bfeefe874cf0db4a8ad95352d52361a1b0a70f
philippe <philippe@b35155ea-a742-4b26-8089-0891bb790021>
2009-04-29 12:29:38 +0000

    Changed description
    git-svn-id: https://svn.dotclear.net/lab@1114 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/lastImages/_define.php
+++ plugins/lastImages/_define.php
@@ -1,7 +1,7 @@
 <?php
 $this->registerModule(
 	/* Name */			"lastImages",
-	/* Description*/		"Get entries first images",
+	/* Description*/		"Display images from posts with different image sizes. Can be randomly chosen and/or filtered by category.",
 	/* Author */		"Philippe",
 	/* Version */			'1.3',
 	/* Permissions */		'usage,contentadmin'
