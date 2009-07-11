2fd14ddaf42a8c2a4ebb2fb5cf57165cf832d680
hadrien <hadrien@b35155ea-a742-4b26-8089-0891bb790021>
2009-07-11 17:04:15 +0000

    v1.2
    git-svn-id: https://svn.dotclear.net/lab@1311 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/twitterPost/_define.php
+++ plugins/twitterPost/_define.php
@@ -27,6 +27,6 @@ $this->registerModule(
 	/* Name */			"TwitterPost",
 	/* Description*/	"Update your status on new posts",
 	/* Author */		"Hadrien Lanneau (http://www.alti.info)",
-	/* Version */		'1.1',
+	/* Version */		'1.2',
 	/* Permissions */ 	'usage,contentadmin'
 );
