83763e9e7ae2694ad7b0c689814518409d951c9f
kozlika <kozlika@b35155ea-a742-4b26-8089-0891bb790021>
2009-10-25 20:50:05 +0000

    newsletter: oops (thanks to Moe mais j'ai un gros doute sur deactivation (en) = désactivation (fr). Yapa mieux ?
    git-svn-id: https://svn.dotclear.net/lab@1742 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/newsletter/trunk/inc/class.newsletter.subscribers.php
+++ plugins/newsletter/trunk/inc/class.newsletter.subscribers.php
@@ -159,7 +159,7 @@ class newsletterSubscribersList extends adminGenericList
 						__('Activation') => 'sendenable',
 						__('Confirmation') => 'sendconfirm',
 						__('Suspension') => 'sendsuspend',
-						__('Desactivation') => 'senddisable'
+						__('Deactivation') => 'senddisable'
 					);
 			
 					$combo_action[__('Changing state')] = array(
@@ -174,7 +174,7 @@ class newsletterSubscribersList extends adminGenericList
 						__('Newsletter') => 'send',
 						__('Activation') => 'sendenable',
 						__('Confirmation') => 'sendconfirm',
-						__('Desactivation') => 'senddisable'
+						__('Deactivation') => 'senddisable'
 					);		
 			
 					$combo_action[__('Changing state')] = array(
