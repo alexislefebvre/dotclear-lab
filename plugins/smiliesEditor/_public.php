4d7174aa99ab54747b376ec6fb3a1a18a11c6799
Osku <Osku@b35155ea-a742-4b26-8089-0891bb790021>
2009-10-29 07:55:59 +0000

    coin
    git-svn-id: https://svn.dotclear.net/lab@1804 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/smiliesEditor/_public.php
+++ plugins/smiliesEditor/_public.php
@@ -32,11 +32,7 @@ class smiliesBehavior
 		
 	         $js = html::stripHostURL($core->blog->getQmarkURL().'pf=smiliesEditor/js/smile.js');
 	
-		echo "\n".
-		//'<script type="text/javascript">'.
-		//'function smiliesManagerInsertSmiley(textarea, smiley) { smiley = \' \'+smiley+\' \'; textarea = document.getElementById(textarea); textarea.focus(); var start, end, scrollPos; if (typeof(document["selection"]) != \'undefined\') { document.selection.createRange().text = smiley; textarea.caretPos += smiley.length; } else if (typeof(textarea[\'setSelectionRange\']) != \'undefined\') { start = textarea.selectionStart; end = textarea.selectionEnd; scrollPos = textarea.scrollTop; textarea.value = textarea.value.substring(0, start)+smiley+textarea.value.substring(end); textarea.setSelectionRange(start + smiley.length, start + smiley.length); textarea.scrollTop = scrollPos; } }'.
-		//'</script>'."\n".
-		'<script type="text/javascript" src="'.$js.'"></script>'."\n";
+		echo "\n".'<script type="text/javascript" src="'.$js.'"></script>'."\n";
 	}
 	
 	public static function publicCommentFormAfterContent()
