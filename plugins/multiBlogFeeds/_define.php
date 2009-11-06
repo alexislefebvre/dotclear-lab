b07f62a18ce56ff35ed2dcc15e6561c97f873789
johan <johan@b35155ea-a742-4b26-8089-0891bb790021>
2009-11-06 16:25:29 +0000

    Toujours une ébauche mais quelques corrections : mise en forme des billets en mode wiki, flux valide, mise en place des bloc de licence, on n'affiche pas les contenus billets avec mot de passe
    git-svn-id: https://svn.dotclear.net/lab@1860 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/multiBlogFeeds/_define.php
+++ plugins/multiBlogFeeds/_define.php
@@ -1,11 +1,23 @@
 <?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of multiBlogFeeds, a plugin for Dotclear 2.
+#
+# Copyright (c) 2009 Johan Pustoch and contributors
+# johan.pustoch@crdp.ac-versailles.fr
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+
 if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
-        /* Name */                      "Rss feed of all blogs",
-        /* Description*/                "Rss feed of all blogs (posts or comments)",
-        /* Author */                    "Johan Pustoch",
-        /* Version */                   '0.1',
-        /* Permissions */               'usage'
+    /* Name */                      "Rss feed of all blogs",
+    /* Description*/                "Rss feed of all blogs (posts or comments)",
+    /* Author */                    "Johan Pustoch",
+    /* Version */                   '0.2',
+    /* Permissions */               'usage'
 );
 ?>
--- plugins/multiBlogFeeds/_public.php
+++ plugins/multiBlogFeeds/_public.php
@@ -1,88 +1,125 @@
 <?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of multiBlogFeeds, a plugin for Dotclear 2.
+#
+# Copyright (c) 2009 Johan Pustoch and contributors
+# johan.pustoch@crdp.ac-versailles.fr
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+
 if (!defined('DC_RC_PATH')) { return; }
 
-header('Content-Type: application/rss+xml');
+header('Content-Type: application/rss+xml, charset=utf-8');
 
 $core->url->register('multiBlogFeedPosts','multiBlogFeedPosts','^multiBlogFeedPosts$',array('RssPostURL','multiBlogFeedPosts'));
 $core->url->register('multiBlogFeedComments','multiBlogFeedComments','^multiBlogFeedComments$',array('RssCommentURL','multiBlogFeedComments'));
-
 
 class RssPostURL extends dcUrlHandlers
 {
         public static function multiBlogFeedPosts($args)
         {
-                AfficheEntete("Nouveaux billets");
-                ListNewPosts($core);
-                AfficheBas();
+                GetRSSHeader("Nouveaux billets","Posts");
+                GetPostsItems($core);
+                GetRSSFooter();
         }
 }
+
 class RssCommentURL extends dcUrlHandlers
 {
         public static function multiBlogFeedComments($args)
         {
-                AfficheEntete("Nouveaux commentaires");
-                ListNewComments($core);
-                AfficheBas();
+                GetRSSHeader("Nouveaux commentaires","Comments");
+                GetCommentsItems($core);
+                GetRSSFooter();
         }
 }
 
-
-function ListNewPosts($core)
+function GetPostsItems($core)
 {
-      $query = "SELECT post_url, post_title, post_content, post_dt , blog_url FROM ".DC_DBPREFIX."post as post,".DC_DBPREFIX."blog as blog  WHERE post.blog_id=blog.blog_id AND post_status = 1 AND post_type = 'post' ORDER BY post_dt DESC LIMIT 50";
-      //echo $query;
-      global $core;
-      $res_post = $core->con->select($query);
-      while ($res_post->fetch())
-      {
-      	$blog_url = $res_post->f('blog_url');
-      	$post_url = $blog_url.'post/'.$res_post->f('post_url');
-        echo '<item>';
-        echo '<title>'.$res_post->f('post_title').'</title>';
-        echo '<link>'.$post_url.'</link>';
-        echo '<pubDate>'.$res_post->f('post_dt').'</pubDate>';
-        echo '<description>'.html::absoluteURLs(html::escapeHTML($res_post->f('post_content')),$core->blog->url).'</description>';
-        echo '</item>';
-      }
+	$query = "SELECT post_url, post_title, post_password,post_words ,post_excerpt,post_excerpt_xhtml,post_content,post_content_xhtml, post_dt,post_tz , post_format , blog_url FROM ".DC_DBPREFIX."post as post,".DC_DBPREFIX."blog as blog  WHERE post.blog_id=blog.blog_id AND post_status = 1 AND post_type = 'post' ORDER BY post_dt DESC LIMIT 50";
+	global $core;
+	$res_post = $core->con->select($query);
+	while ($res_post->fetch())
+	{
+		$blog_url = $res_post->f('blog_url');
+		$post_url = $blog_url.'post/'.$res_post->f('post_url');
+		echo '<item>';
+		echo '<title>'.$res_post->f('post_title').'</title>';
+		echo '<guid isPermaLink="true">'.$post_url.'</guid>';
+		echo '<link>'.$post_url.'</link>';
+		echo '<pubDate>'.dt::rfc822(strtotime($res_post->f('post_dt')),$res_post->f('post_tz')).'</pubDate>';
+		if ( $res_post->f('post_format')=='wiki' )
+		{
+			if ($res_post->f('post_password')!="")
+			echo '<description>Billet en accès privé</description>';
+			else echo '<description>'.html::absoluteURLs(html::escapeHTML( $core->wikiTransform( $res_post->f('post_content') ) ) , $core->blog->url ).'</description>';
+		}
+		else
+		{
+			if ($res_post->f('post_password')!="")
+			echo '<description>Billet en accès privé</description>';
+			else echo '<description>'.html::absoluteURLs(html::escapeHTML( $res_post->f('post_content_xhtml')), $core->blog->url ).'</description>';
+		}
+		echo '</item>';
+	}
 }
 
-function ListNewComments($core)
+function GetCommentsItems($core)
 {
-      $query = "SELECT post_url, post_title, comment_content, comment_dt , blog_url FROM ".DC_DBPREFIX."comment as comment,".DC_DBPREFIX."post as post,".DC_DBPREFIX."blog as blog  WHERE post.post_id=comment.post_id AND post.blog_id=blog.blog_id AND comment_status = 1 ORDER BY comment_dt DESC LIMIT 50";
-      global $core;
-      $res_post = $core->con->select($query);
-      while ($res_post->fetch())
-      {
-      	$blog_url = $res_post->f('blog_url');
-      	$post_url = $blog_url.'post/'.$res_post->f('post_url');
-        echo '<item>';
-        echo '<title>'.$res_post->f('post_title').'</title>';
-        echo '<link>'.$post_url.'</link>';
-        echo '<pubDate>'.$res_post->f('comment_dt').'</pubDate>';
-        echo '<description>'.html::absoluteURLs(html::escapeHTML($res_post->f('comment_content')),$core->blog->url).'</description>';
-        echo '</item>';
-      }
+	$query = "SELECT post_url, post_title,post_password, comment_content, comment_id, comment_dt ,comment_tz, blog_url FROM ".DC_DBPREFIX."comment as comment,".DC_DBPREFIX."post as post,".DC_DBPREFIX."blog as blog  WHERE post.post_id=comment.post_id AND post.blog_id=blog.blog_id AND comment_status = 1 ORDER BY comment_dt DESC LIMIT 50";
+	global $core;
+	$res_post = $core->con->select($query);
+	while ($res_post->fetch())
+	{
+		$blog_url = $res_post->f('blog_url');
+		$post_url = $blog_url.'post/'.$res_post->f('post_url');
+		$comment_id = $res_post->f('comment_id');
+		echo '<item>';
+		echo '<title>'.$res_post->f('post_title').'</title>';
+		echo '<guid isPermaLink="true">'.$post_url.'#c'.$comment_id.'</guid>';
+		echo '<link>'.$post_url.'#c'.$comment_id.'</link>';
+		echo '<pubDate>'.dt::rfc822(strtotime($res_post->f('comment_dt')),$res_post->f('comment_tz')).'</pubDate>';
+		if ( $res_post->f('post_format')=='wiki' )
+		{
+			if ($res_post->f('post_password')!="")
+			echo '<description>Billet en accès privé</description>';
+			else echo '<description>'.html::absoluteURLs(html::escapeHTML( $core->wikiTransform( $res_post->f('comment_content') ) ) , $core->blog->url ).'</description>';
+		}
+		else
+		{
+			if ($res_post->f('post_password')!="")
+			echo '<description>Billet en accès privé</description>';
+			else echo '<description>'.html::absoluteURLs(html::escapeHTML( $res_post->f('comment_content')), $core->blog->url ).'</description>';
+		}
+		echo '</item>';
+	}
 }
 
-function AfficheEntete($type)
+function GetRSSHeader( $texte , $type)
 {
 	global $core;
-  echo '<?xml version="1.0" encoding="utf-8"?><?xml-stylesheet title="XSL formatting" type="text/xsl" href="'.$core->blog->url.'feed/rss2/xslt" ?><rss version="2.0"
+	echo '<?xml version="1.0" encoding="utf-8"?><?xml-stylesheet title="XSL formatting" type="text/xsl" href="'.$core->blog->url.'feed/rss2/xslt" ?><rss version="2.0"
   xmlns:dc="http://purl.org/dc/elements/1.1/"
   xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-  xmlns:content="http://purl.org/rss/1.0/modules/content/">
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:atom="http://www.w3.org/2005/Atom" >
   <channel>
-  <title>Feed - '.$type.' - '.$core->blog->name.'</title>
+  <title>Feed - '.$texte.' - '.$core->blog->name.'</title>
   <link>'.$core->blog->url.'</link>
   <description>'.html::escapeHTML($core->blog->desc).'</description>
   <language>fr</language>
-  <pubDate>'.date('D, j F Y H:i:s').'</pubDate>
-  <generator>Dotclear</generator>';
- }
+  <pubDate>'.date(DateTime::RFC822).'</pubDate>
+  <generator>Dotclear - MultiBlogFeeds</generator>
+  <atom:link href="'.$core->blog->url.'multiBlogFeed'.$type.'" rel="self" type="application/rss+xml" />';
+}
 
-function AfficheBas()
+function GetRSSFooter()
 {
-echo '</channel>
-</rss>';
+	echo '</channel>
+	</rss>';
 }
 ?>
