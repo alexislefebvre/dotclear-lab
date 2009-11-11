72c5ee4a7fd652bbe12ac07139a72d38864a2c2b
annso <annso@b35155ea-a742-4b26-8089-0891bb790021>
2009-11-11 21:23:18 +0000

    shortArchives : localisation (closes #314)
    git-svn-id: https://svn.dotclear.net/lab@1874 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/shortArchives/_widgets.php
+++ plugins/shortArchives/_widgets.php
@@ -17,7 +17,7 @@ class shortArchivesWidgets
 	public static function initWidgets(&$w)
 	{
 		$w->create('shortArchives',__('Short Archives'), array('tplShortArchives','shortArchivesWidgets'));
-		$w->shortArchives->setting('title',__('Title:'),('Archives'));
+		$w->shortArchives->setting('title',__('Title'),('Archives'));
 		$w->shortArchives->setting('postcount',__('With entries counts'),1,'check');
 		$w->shortArchives->setting('homeonly',__('Home page only'),0,'check');
 	}
