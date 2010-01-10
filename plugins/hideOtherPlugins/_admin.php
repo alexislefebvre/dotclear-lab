7b58b6bde4034d18bd8a759bacafd1081dc08bca
Moe <Moe@b35155ea-a742-4b26-8089-0891bb790021>
2010-01-10 17:52:48 +0000

    added ''Hide Other Plugins'' plugin
    git-svn-id: https://svn.dotclear.net/lab@2017 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/hideOtherPlugins/_admin.php
@@ -0,0 +1,78 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+# This file is part of Hide Other Plugins, a plugin for Dotclear 2
+# Copyright 2010 Moe (http://gniark.net/)
+#
+# Hide Other Plugins is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License v2.0
+# as published by the Free Software Foundation.
+#
+# Hide Other Plugins is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# ***** END LICENSE BLOCK *****
+
+if (!defined('DC_CONTEXT_ADMIN')) {exit;}
+
+$core->addBehavior('adminPageHTMLHead',
+	array('hideOtherPlugins','adminPageHTMLHead'));
+
+class hideOtherPlugins
+{
+	public static function removeOtherPlugins($items)
+	{
+		$plugins = array('antispam','importExport','aboutConfig','blogroll',
+			'maintenance','pings','pages','metadata','widgets');
+		
+		if (preg_match('/plugin.php\?p=(\w+)/',
+			$_SERVER['REQUEST_URI'],$plugin))
+		{
+			# ignore Pages, Tags and Widgets
+			if (in_array($plugin[1],$plugins))
+			{
+				return($items);
+			}
+			else
+			{
+				$plugins[] = $plugin[1];
+			}
+		}
+		else
+		{
+			return($items);
+		}
+		
+		$new_items = array();
+		
+		foreach ($items as $key => $item)
+		{
+			preg_match('/href="plugin.php\?p=(.*?)"/',$item,$plugin);
+			$plugin = $plugin[1];
+			
+			if (in_array($plugin,$plugins))
+			{
+				$new_items[] = $item;
+			}
+		}
+		
+		return($new_items);
+	}
+	
+	public static function adminPageHTMLHead()
+	{
+		global $_menu;
+		
+		$_menu['Plugins']->items =
+			self::removeOtherPlugins($_menu['Plugins']->items);
+	}
+
+}
+
+?>
---
+++ plugins/hideOtherPlugins/_define.php
@@ -0,0 +1,31 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+# This file is part of Hide Other Plugins, a plugin for Dotclear 2
+# Copyright 2010 Moe (http://gniark.net/)
+#
+# Hide Other Plugins is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License v2.0
+# as published by the Free Software Foundation.
+#
+# Hide Other Plugins is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# ***** END LICENSE BLOCK *****
+
+if (!defined('DC_RC_PATH')) {return;}
+
+$this->registerModule(
+	/* Name */					'Hide Other Plugins',
+	/* Description */		'Hide other plugins',
+	/* Author */				'Moe (http://gniark.net/)',
+	/* Version */				'0.1',
+	/* Permissions */		'usage,contentadmin'
+);
+?>
