91501f29d196cbfafd2d555099d517646f403f57
bruno <bruno@b35155ea-a742-4b26-8089-0891bb790021>
2010-03-10 18:27:12 +0000

    Mystique theme, v1.0
    git-svn-id: https://svn.dotclear.net/lab@2120 b35155ea-a742-4b26-8089-0891bb790021

---
+++ themes/mystique/_define.php
@@ -0,0 +1,20 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# This file is part of Dotclear 2.
+#
+# Copyright (c) 2003-2009 Olivier Meunier and contributors
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			"Mystique",
+	/* Description*/		"Mystique Theme, adapted from WordPress",
+	/* Author */			"Digitalnature, ported to DC2 by Dsls",
+	/* Version */			'1.0'
+);
+?>
---
+++ themes/mystique/_public.php
@@ -0,0 +1,16 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# This file is part of Noviny, a Dotclear 2 theme.
+#
+# Copyright (c) 2003-2008 Olivier Meunier and contributors
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/public');
+
+?>
