ac1ef9fab8f7b42797b0c7a799de817b9c19a56a
Osku <Osku@b35155ea-a742-4b26-8089-0891bb790021>
2010-04-13 06:46:25 +0000

    Plugin '''agora''' - O.5.8 :  * category combobox now before thread subject in newthread.html
    git-svn-id: https://svn.dotclear.net/lab@2182 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/agora/_define.php
+++ plugins/agora/_define.php
@@ -17,7 +17,7 @@ $this->registerModule(
 	/* Name */		"Agora",
 	/* Description*/	"Stuff to build a forum",
 	/* Author */		"Osku, Tomtom (http://blog.zenstyle.fr) and contributors",
-	/* Version */		'0.5.7',
+	/* Version */		'0.5.8',
 	/* Permissions */	'admin'
 );
 ?>
