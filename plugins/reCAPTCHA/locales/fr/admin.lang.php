48733c365c47df6ed2d8a73fa4bfec4d454c8ecb
Moe <Moe@b35155ea-a742-4b26-8089-0891bb790021>
2010-04-15 20:30:43 +0000

    '''reCAPTCHA''' :  * updated locales
    git-svn-id: https://svn.dotclear.net/lab@2193 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/reCAPTCHA/locales/fr/admin.lang.php
+++ plugins/reCAPTCHA/locales/fr/admin.lang.php
@@ -1,40 +1,40 @@
 <?php
 // Language: français 
 // Module: reCAPTCHA - 0.1
-// Date: 2009-11-22 21:20:25 
+// Date: 2009-11-22 21:57:56 
 // Translated with dcTranslater - 1.2 
 
-#inc/class.dc.filter.reCAPTCHA.php:115
+#inc/class.dc.filter.reCAPTCHA.php:112
 $GLOBALS['__l10n']['Red'] = 'Rouge';
 
-#inc/class.dc.filter.reCAPTCHA.php:116
+#inc/class.dc.filter.reCAPTCHA.php:113
 $GLOBALS['__l10n']['White'] = 'Blanc';
 
-#inc/class.dc.filter.reCAPTCHA.php:117
+#inc/class.dc.filter.reCAPTCHA.php:114
 $GLOBALS['__l10n']['Blackglass'] = 'Verre noir';
 
-#inc/class.dc.filter.reCAPTCHA.php:118
+#inc/class.dc.filter.reCAPTCHA.php:115
 $GLOBALS['__l10n']['Clean'] = 'Net';
 
-#inc/class.dc.filter.reCAPTCHA.php:126
+#inc/class.dc.filter.reCAPTCHA.php:123
 $GLOBALS['__l10n']['Configuration successfully updated.'] = 'Configuration mise à jour avec succès.';
 
-#inc/class.dc.filter.reCAPTCHA.php:130
+#inc/class.dc.filter.reCAPTCHA.php:127
 $GLOBALS['__l10n']['reCAPTCHA keys are required to use the reCAPTCHA service'] = 'Des clés reCAPTCHA sont nécessaires pour utiliser le service reCAPTCHA';
 
-#inc/class.dc.filter.reCAPTCHA.php:136
+#inc/class.dc.filter.reCAPTCHA.php:133
 $GLOBALS['__l10n']['Get reCAPTCHA keys'] = 'Obtenir des clés reCAPTCHA';
 
-#inc/class.dc.filter.reCAPTCHA.php:143
+#inc/class.dc.filter.reCAPTCHA.php:140
 $GLOBALS['__l10n']['reCAPTCHA public key:'] = 'Clé publique reCAPTCHA&nbsp;:';
 
-#inc/class.dc.filter.reCAPTCHA.php:150
+#inc/class.dc.filter.reCAPTCHA.php:147
 $GLOBALS['__l10n']['reCAPTCHA private key:'] = 'Clé privée reCAPTCHA&nbsp;:';
 
-#inc/class.dc.filter.reCAPTCHA.php:157
+#inc/class.dc.filter.reCAPTCHA.php:154
 $GLOBALS['__l10n']['reCAPTCHA theme:'] = 'Thème reCAPTCHA&nbsp;:';
 
-#inc/class.dc.filter.reCAPTCHA.php:164
+#inc/class.dc.filter.reCAPTCHA.php:161
 $GLOBALS['__l10n']['Save configuration'] = 'Enregistrer la configuration';
 
 ?>
--- plugins/reCAPTCHA/locales/fr/main.lang.php
+++ plugins/reCAPTCHA/locales/fr/main.lang.php
@@ -1,10 +1,10 @@
 <?php
 // Language: français 
 // Module: reCAPTCHA - 0.1
-// Date: 2009-11-22 21:20:25 
+// Date: 2009-11-22 21:57:56 
 // Translated with dcTranslater - 1.2 
 
-#inc/class.dc.filter.reCAPTCHA.php:35
-$GLOBALS['__l10n']['Check comments with reCAPTCHA'] = 'Vérifier les commentaires avec';
+#inc/class.dc.filter.reCAPTCHA.php:34
+$GLOBALS['__l10n']['Check comments with reCAPTCHA'] = 'Vérifier les commentaires avec reCAPTCHA';
 
 ?>
