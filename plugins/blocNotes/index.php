77eaf1e3a9a6625a099e1effa41d53ef566fec13
Moe <Moe@b35155ea-a742-4b26-8089-0891bb790021>
2010-04-15 22:02:44 +0000

    '''Bloc-Notes''' : [2195] was uncomplete
    git-svn-id: https://svn.dotclear.net/lab@2196 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/blocNotes/index.php
+++ plugins/blocNotes/index.php
@@ -2,7 +2,7 @@
 # ***** BEGIN LICENSE BLOCK *****
 #
 # This file is part of Bloc-Notes.
-# Copyright 2008,2009 Moe (http://gniark.net/)
+# Copyright 2008,2009,2010 Moe (http://gniark.net/)
 #
 # Bloc-Notes is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
