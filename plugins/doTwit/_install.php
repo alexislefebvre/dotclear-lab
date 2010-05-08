8d11411bb9e6468da5dd14bcd4e0adf0758e7ee9
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2010-05-08 14:22:26 +0000

    pb encodage du fichier
    git-svn-id: https://svn.dotclear.net/lab@2231 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/doTwit/_install.php
+++ plugins/doTwit/_install.php
@@ -16,13 +16,13 @@ $m_version = $core->plugins->moduleInfo('DotFreeWidget','1');
 # On lit la version du plugin dans la table des versions
 $i_version = $core->getVersion('1');
  
-# La version dans la table est supérieure ou égale à
+# La version dans la table est supÃ©rieure ou Ã©gale Ã 
 # celle du module, on ne fait rien puisque celui-ci
-# est installé
+# est installÃ©
 if (version_compare($i_version,$m_version,'>=')) {
 	return;
 }
  
-# La procédure d'installation commence vraiment là
+# La procÃ©dure d'installation commence vraiment lÃ 
 $core->setVersion('doTwit',$m_version);
 ?>
