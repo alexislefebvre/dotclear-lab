7af33f4cce3fbbe4d8db500648afc29a295d6a2a
JcDenis <JcDenis@b35155ea-a742-4b26-8089-0891bb790021>
2010-05-24 00:36:46 +0000

    '''postInfoWidget 0.1''':  * First lab release
    git-svn-id: https://svn.dotclear.net/lab@2254 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/postInfoWidget/locales/fr/main.lang.php
@@ -0,0 +1,131 @@
+<?php
+// Language: français 
+// Module: postInfoWidget - 0.1
+// Date: 2010-05-23 23:46:22 
+// Translated with dcTranslater - 1.3 
+
+#_widgets.php:24
+$GLOBALS['__l10n']['Entry information list'] = 'Liste d\'information du billet';
+
+#_widgets.php:27
+$GLOBALS['__l10n']['About this entry'] = 'Á propos de ce billet';
+
+#_widgets.php:29
+$GLOBALS['__l10n']['Publish date text:'] = 'Texte pour la date de publication :';
+
+#_widgets.php:30
+$GLOBALS['__l10n']['Publish on %Y-%m-%d %H:%M'] = 'Publié le %d/%m/%Y à %H:%M';
+
+#_widgets.php:32
+$GLOBALS['__l10n']['Create date text:'] = 'Texte pour la date de création :';
+
+#_widgets.php:33
+$GLOBALS['__l10n']['Create on %Y-%m-%d %H:%M'] = 'Créé le %d/%m/%Y à %H:%M';
+
+#_widgets.php:35
+$GLOBALS['__l10n']['Update date text:'] = 'Texte pour la date de dernière mise à jour :';
+
+#_widgets.php:36
+$GLOBALS['__l10n']['Update on %Y-%m-%d %H:%M'] = 'Mis à jour le %d/%m/%Y à %H:%M';
+
+#_widgets.php:38
+$GLOBALS['__l10n']['Language: (%T = name, %C = code, %F = flag)'] = 'Text pour la langue : (%T = nom, %C = code, %F = drapeau)';
+
+#_widgets.php:39
+$GLOBALS['__l10n']['Language: %T %F'] = 'Langue : %T %F';
+
+#_widgets.php:41
+$GLOBALS['__l10n']['Author text: (%T = author)'] = 'Texte pour l\'auteur : (%T = auteur)';
+
+#_widgets.php:42
+$GLOBALS['__l10n']['Author: %T'] = 'Auteur : %T';
+
+#_widgets.php:44
+$GLOBALS['__l10n']['Category text: (%T = category)'] = 'Texte pour la catégorie : (%T = catégorie)';
+
+#_widgets.php:45
+$GLOBALS['__l10n']['Category: %T'] = 'Catégorie : %T';
+
+#_widgets.php:48
+$GLOBALS['__l10n']['Tags text: (%T = tags list)'] = 'Text pour les tags : (%T = liste des tags)';
+
+#_widgets.php:49
+$GLOBALS['__l10n']['Tags: %T'] = 'Tags : %T';
+
+#_widgets.php:52
+$GLOBALS['__l10n']['Attachments text: (%T = text, %D = numeric)'] = 'Texte pour le nombre d\'annexes : (%T = texte, %D = nombre)';
+
+#_widgets.php:53
+$GLOBALS['__l10n']['Attachments: %T'] = 'Annexes : %T';
+
+#_widgets.php:55
+$GLOBALS['__l10n']['Comments text: (%T = text, %D = numeric)'] = 'Texte pour le nombre de commentaires : (%T = texte, %D = nombre)';
+
+#_widgets.php:56
+$GLOBALS['__l10n']['Comments: %T'] = 'Commentaires : %T';
+
+#_widgets.php:58
+$GLOBALS['__l10n']['Trackbacks text: (%T = text, %D = numeric)'] = 'Texte pour le nombre de rétroliens: (%T = texte, %D = nombre)';
+
+#_widgets.php:59
+$GLOBALS['__l10n']['Trackbacks: %T'] = 'Rétroliens : %T';
+
+#_widgets.php:61
+$GLOBALS['__l10n']['Permalink text: (%T = text link, %F = full link)'] = 'Texte pour le lien pemanent : (%T = texte, %F = complet)';
+
+#_widgets.php:62
+#_widgets.php:67
+#_widgets.php:70
+#_widgets.php:73
+#_widgets.php:76
+$GLOBALS['__l10n']['%T'] = '%T';
+
+#_widgets.php:64
+$GLOBALS['__l10n']['Show comment feed url'] = 'Afficher l\'URL du flux des commentaires';
+
+#_widgets.php:66
+$GLOBALS['__l10n']['Link to previous entry: (%T = navigation text, %F = entry title)'] = 'Lien vers le billet précédent : (%T = texte de navigation, %F = titre du billet)';
+
+#_widgets.php:69
+$GLOBALS['__l10n']['Link to next entry: (%T = navigation text, %F = entry title)'] = 'Lien vers le billet suivant : (%T = texte de navigation, %F = titre du billet)';
+
+#_widgets.php:72
+$GLOBALS['__l10n']['Link to previous entry of this category: (%T = navigation text, %F = entry title)'] = 'Lien vers le billet précédent de la catégorie : (%T = texte de navigation, %F = titre du billet)';
+
+#_widgets.php:75
+$GLOBALS['__l10n']['Link to next entry of this category: (%T = navigation text, %F = entry title)'] = 'Lien vers le billet suivant de la catégorie: (%T = texte de navigation, %F = titre du billet)';
+
+#_widgets.php:78
+$GLOBALS['__l10n']['Try to adapt style'] = 'Essayer d\'adapter le style :';
+
+#_widgets.php:79
+$GLOBALS['__l10n']['No style'] = 'sans style';
+
+#_widgets.php:80
+$GLOBALS['__l10n']['Small icon'] = 'petite icône';
+
+#_widgets.php:81
+$GLOBALS['__l10n']['Normal icon'] = 'icône normale';
+
+#_widgets.php:83
+$GLOBALS['__l10n']['Try to remove entry information'] = 'Essayer de cacher les information du billet';
+
+#_widgets.php:85
+$GLOBALS['__l10n']['Try to remove entry tags'] = 'Essayer de cacher la liste des tags du billet';
+
+#_widgets.php:87
+$GLOBALS['__l10n']['Try to remove entry navigation'] = 'Essayer de cacher les liens de navigation';
+
+#_widgets.php:258
+$GLOBALS['__l10n']['Previous entry'] = 'Billet précédent';
+
+#_widgets.php:267
+$GLOBALS['__l10n']['Next entry'] = 'Billet suivant';
+
+#_widgets.php:276
+$GLOBALS['__l10n']['Previous entry of this category'] = 'Billet précédent de cette catégorie';
+
+#_widgets.php:285
+$GLOBALS['__l10n']['Next entry of this category'] = 'Billet suivant de cette catégorie';
+
+?>
