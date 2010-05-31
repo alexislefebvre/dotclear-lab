bca9c604251cb1e6f5e8115e4ccc4fc2192b48c8
Moe <Moe@b35155ea-a742-4b26-8089-0891bb790021>
2010-05-31 20:19:04 +0000

    added '''Widgets to Template (0.1)''' plugin
    git-svn-id: https://svn.dotclear.net/lab@2280 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/widgets2Tpl/_define.php
@@ -0,0 +1,34 @@
+<?php 
+# ***** BEGIN LICENSE BLOCK *****
+#
+# This file is part of Widgets to Template, a plugin for Dotclear 2
+# Copyright 2010 Moe (http://gniark.net/)
+#
+# Widgets to Template is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License v2.0
+# as published by the Free Software Foundation.
+#
+# Widgets to Template is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# Icon (icon.png) and images are from Silk Icons :
+# <http://www.famfamfam.com/lab/icons/silk/>
+#
+# ***** END LICENSE BLOCK *****
+
+if (!defined('DC_RC_PATH')) {return;}
+
+$this->registerModule(
+  /* Name */				'Widgets to Template',
+  /* Description*/	'Get template code for your widgets',
+  /* Author */			'Moe (http://gniark.net/)',
+  /* Version */			'0.1',
+  /* Permissions */	'admin'
+);
+?>
