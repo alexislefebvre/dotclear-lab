80bbf842a936a8755500e1d1bfab4588930472f7
JcDenis <JcDenis@b35155ea-a742-4b26-8089-0891bb790021>
2010-06-05 22:50:39 +0000

    '''efiMetadatas 0.3'''  * Switched to DC 2.2
    git-svn-id: https://svn.dotclear.net/lab@2304 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/efiMetadatas/_widgets.php
+++ plugins/efiMetadatas/_widgets.php
@@ -2,7 +2,7 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of efiMetadatas, a plugin for Dotclear 2.
 # 
-# Copyright (c) 2009 JC Denis and contributors
+# Copyright (c) 2009-2010 JC Denis and contributors
 # jcdenis@gdwd.com
 # 
 # Licensed under the GPL version 2.0 license.
@@ -19,7 +19,7 @@ class efiMetadatasWidget
 	public static function adminEFIM($w)
 	{
 		global $core;
-
+		
 		$categories_combo = array('-' => '', __('Uncategorized') => 'null');
 		$categories = $core->blog->getCategories();
 		while($categories->fetch())
@@ -27,7 +27,7 @@ class efiMetadatasWidget
 			$cat_title = html::escapeHTML($categories->cat_title);
 			$categories_combo[$cat_title] = $categories->cat_id;
 		}
-
+		
 		$thumbnail_combo = array(
 			'-' => '',
 			__('square') => 'sq',
@@ -35,7 +35,7 @@ class efiMetadatasWidget
 			__('small') => 's',
 			__('medium') => 'm'
 		);
-
+		
 		$w->create('efim',
 			__('Entry first image metadatas'),array('efiMetadatasWidget','publicEFIM')
 		);
@@ -52,33 +52,33 @@ class efiMetadatasWidget
 			__('Show empty metadatas'),0,'check'
 		);
 	}
-
+	
 	public static function publicEFIM($w)
 	{
 		global $core, $_ctx; 
-
+		
 		# Not in post context
 		if (!$_ctx->exists('posts') || !$_ctx->posts->post_id) return;
-
+		
 		# Not supported post type
 		if (!in_array($_ctx->posts->post_type,array('post','gal','galitem'))) return '';
-
+		
 		# Category limit
 		if ($w->category == 'null' && $_ctx->posts->cat_id !== null) return;
 		if ($w->category != 'null' && $w->category != '' && $w->category != $_ctx->posts->cat_id) return;
-
+		
 		# Content lookup
 		$text = $_ctx->posts->post_excerpt_xhtml.$_ctx->posts->post_content_xhtml;
-
+		
 		# Find source image
 		$img = efiMetadatas::imgSource($core,$text,$w->thumbsize);
-
+		
 		# No image
 		if (!$img['source']) return;
-
+		
 		# List metas
 		$metas = efiMetadatas::imgMeta($core,$img['source']);
-
+		
 		$content = '';
 		foreach($metas as $k => $v)
 		{
@@ -86,10 +86,10 @@ class efiMetadatasWidget
 			if (!$w->showmeta && empty($v[1])) continue;
 			$content .= '<li class="efi-'.$k.'"><strong>'.$v[0].':</strong><br />'.$v[1].'</li>';
 		}
-
+		
 		# No meta
 		if (empty($content)) return;
-
+		
 		# thumbnail
 		if ($img['thumb'])
 		{
@@ -100,7 +100,7 @@ class efiMetadatasWidget
 			'</div>'.
 			"</div>\n";
 		}
-
+		
 		return 
 		'<div class="entryFirstImageMetas">'.
 		($w->title ? '<h2>'.html::escapeHTML($w->title).'</h2>' : '').
--- plugins/efiMetadatas/inc/class.efimetadatas.php
+++ plugins/efiMetadatas/inc/class.efimetadatas.php
@@ -2,7 +2,7 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of efiMetadatas, a plugin for Dotclear 2.
 # 
-# Copyright (c) 2009 JC Denis and contributors
+# Copyright (c) 2009-2010 JC Denis and contributors
 # jcdenis@gdwd.com
 # 
 # Licensed under the GPL version 2.0 license.
@@ -10,27 +10,26 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # -- END LICENSE BLOCK ------------------------------------
 
-
 class efiMetadatas
 {
 	public static function imgSource($core,$subject,$size='')
 	{
 		# Path and url
-		$p_url = $core->blog->settings->public_url;
+		$p_url = $core->blog->settings->system->public_url;
 		$p_site = preg_replace('#^(.+?//.+?)/(.*)$#','$1',$core->blog->url);
 		$p_root = $core->blog->public_path;
-
+		
 		# Image pattern
 		$pattern = '(?:'.preg_quote($p_site,'/').')?'.preg_quote($p_url,'/');
 		$pattern = sprintf('/<img.+?src="%s(.*?\.(?:jpg|gif|png))"[^>]+/msu',$pattern);
-
+		
 		# No image
 		if (!preg_match_all($pattern,$subject,$m)) return;
-
+		
 		$src = $thb = $alt = false;
 		$alt = $metas = $thumb = '';
 		$allowed_ext = array('.jpg','.JPG','.png','.PNG','.gif','.GIF');
-
+		
 		# Loop through images
 		foreach ($m[1] as $i => $img)
 		{
@@ -38,16 +37,16 @@ class efiMetadatas
 			$info = path::info($img);
 			$base = $info['base'];
 			$ext = $info['extension'];
-
+			
 			# Not original
 			if (preg_match('/^\.(.+)_(sq|t|s|m)$/',$base,$mbase))
 			{
 				$base = $mbase[1];
 			}
-
+			
 			# Full path
 			$f = $p_root.'/'.$info['dirname'].'/'.$base;
-
+			
 			# Find extension
 			foreach($allowed_ext as $end)
 			{
@@ -57,10 +56,10 @@ class efiMetadatas
 					break;
 				}
 			}
-
+			
 			# No file
 			if (!$src) continue;
-
+			
 			# Find thumbnail
 			if (!empty($size))
 			{
@@ -70,7 +69,7 @@ class efiMetadatas
 					$thb = $p_url.(dirname($img) != '/' ? dirname($img) : '').'/.'.$base.'_'.$size.'.jpg';
 				}
 			}
-
+			
 			# Find image description
 			if (preg_match('/alt="([^"]+)"/',$m[0][$i],$malt))
 			{
@@ -81,7 +80,7 @@ class efiMetadatas
 		
 		return array('source' => $src, 'thumb' => $thb, 'title' => $alt);
 	}
-
+	
 	public static function imgMeta($core,$src)
 	{
 		$metas = array(
@@ -100,7 +99,7 @@ class efiMetadatas
 			'ExposureBiasValue' => array(__('Exposure Bias'),''),
 			'MeteringMode' => array(__('Metering mode'),'')
 		);
-
+		
 		$exp_prog = array(
 			0 => __('Not defined'),
 			1 => __('Manual'),
@@ -112,7 +111,7 @@ class efiMetadatas
 			7 => __('Portait mode'),
 			8 => __('Landscape mode')
 		);
-
+		
 		$met_mod = array(
 			0 => __('Unknow'),
 			1 => __('Average'),
@@ -123,17 +122,17 @@ class efiMetadatas
 			6 => __('Partial'),
 			7 => __('Other')
 		);
-
+		
 		if (!$src || !file_exists($src)) return $metas;
-
+		
 		$m = imageMeta::readMeta($src);
-
+		
 		# Title
 		if (!empty($m['Title']))
 		{
 			$metas['Title'][1] = html::escapeHTML($m['Title']);
 		}
-
+		
 		# Description
 		if (!empty($m['Description']))
 		{
@@ -142,7 +141,7 @@ class efiMetadatas
 				$metas['Description'][1] = html::escpeHTML($m['Description']);
 			}
 		}
-
+		
 		# Location
 		if (!empty($m['City']))
 		{
@@ -160,23 +159,23 @@ class efiMetadatas
 		# DateTimeOriginal
 		if (!empty($m['DateTimeOriginal']))
 		{
-			$dt_ft = $core->blog->settings->date_format.', '.$core->blog->settings->time_format;
-			$dt_tz = $core->blog->settings->blog_timezone;
+			$dt_ft = $core->blog->settings->system->date_format.', '.$core->blog->settings->system->time_format;
+			$dt_tz = $core->blog->settings->system->blog_timezone;
 			$metas['DateTimeOriginal'][1] = dt::dt2str($dt_ft,$m['DateTimeOriginal'],$dt_tz);
 		}
-
+		
 		# Make
 		if (isset($m['Make']))
 		{
 			$metas['Make'][1] = html::escapeHTML($m['Make']);
 		}
-
+		
 		# Model
 		if (isset($m['Model']))
 		{
 			$metas['Model'][1] = html::escapeHTML($m['Model']);
 		}
-
+		
 		# Lens
 		if (isset($m['Lens']))
 		{
@@ -189,46 +188,46 @@ class efiMetadatas
 			$metas['ExposureProgram'][1] = isset($exp_prog[$m['ExposureProgram']]) ?
 			$exp_prog[$m['ExposureProgram']] : $m['ExposureProgram'];
 		}
-
+		
 		# Exposure
 		if (!empty($m['Exposure']))
 		{
 			$metas['Exposure'][1] = $m['Exposure'].'s';
 		}
-
+		
 		# FNumber
 		if (!empty($m['FNumber']))
 		{
 			$ap = sscanf($m['FNumber'],'%d/%d');
 			$metas['FNumber'][1] = $ap ? 'f/'.( $ap[0] / $ap[1]) :	$m['FNumber'];
 		}
-
+		
 		# ISOSpeedRatings
 		if (!empty($m['ISOSpeedRatings']))
 		{
 			$metas['ISOSpeedRatings'][1] = $m['ISOSpeedRatings'];
 		}
-
+		
 		# FocalLength
 		if (!empty($m['FocalLength']))
 		{
 			$fl = sscanf($m['FocalLength'],'%d/%d');
 			$metas['FocalLength'][1] = $fl ? $fl[0]/$fl[1].'mm' : $m['FocalLength'];
 		}
-
+		
 		# ExposureBiasValue
 		if (isset($m['ExposureBiasValue']))
 		{
 			$metas['ExposureBiasValue'][1] = $m['ExposureBiasValue'];
 		}
-
+		
 		# MeteringMode
 		if (isset($m['MeteringMode']))
 		{
 			$metas['MeteringMode'][1] = isset($met_mod[$m['MeteringMode']]) ?
 			$exp_prog[$m['MeteringMode']] : $m['MeteringMode'];
 		}
-
+		
 		return $metas;
 	}
 }
