2275c5ddc306f6aae4eb758da51c0de0636f26d0
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2010-06-08 09:19:59 +0000

    Corrections sur licence
    git-svn-id: https://svn.dotclear.net/lab@2331 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/myPostCategoryIf/_define.php
+++ plugins/myPostCategoryIf/_define.php
@@ -2,13 +2,13 @@
 # ***** BEGIN LICENSE BLOCK *****
 #
 # This file is part of myPostCategoryIf, a plugin for Dotclear 2
-# Copyright 2010 Adjaya, brol
+# Copyright 2010 Adjaya, Pierre Van Glabeke
 #
-# Informations is free software; you can redistribute it and/or
+# myPostCategoryIf is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License v2.0
 # as published by the Free Software Foundation.
 #
-# Informations is distributed in the hope that it will be useful,
+# myPostCategoryIf is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
@@ -23,7 +23,7 @@ if (!defined('DC_RC_PATH')) { return; }
 $this->registerModule(
 	/* Name */			"MyPostCategoryIf",
 	/* Description*/		"Multi post categories filter",
-	/* Author */		"Adjaya, brol",
+	/* Author */		"Adjaya, Pierre Van Glabeke",
 	/* Version */		'0.1',
 	/* Permissions */		'usage,contentadmin'
 );
