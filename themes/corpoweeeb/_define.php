5f3419b638be881f6b856f5cead100233eea49c2
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2010-06-28 15:13:58 +0000

    pb de niveau pour le menu
    git-svn-id: https://svn.dotclear.net/lab@2409 b35155ea-a742-4b26-8089-0891bb790021

--- themes/corpoweeeb/_define.php
+++ themes/corpoweeeb/_define.php
@@ -18,6 +18,6 @@ $this->registerModule(
 	/* Description*/		"Thème corporate",
 	/* Author */			"Weeeb (http://www.weeeb.fr) - Pierre Van Glabeke",
 	/* Version */			'1.0'
-	/* date 20062010 */
+	/* date 28062010 */
 );
 ?>
