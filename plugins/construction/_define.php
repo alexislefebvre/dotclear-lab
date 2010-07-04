810cfad33d003a0a0eab22c56d2bef4168efe6b2
Osku <Osku@b35155ea-a742-4b26-8089-0891bb790021>
2010-07-04 19:04:26 +0000

    Oops
    git-svn-id: https://svn.dotclear.net/lab@2442 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/construction/_define.php
+++ plugins/construction/_define.php
@@ -16,7 +16,7 @@ $this->registerModule(
 	/* Name */		"Construction",
 	/* Description*/	"Put your blog in contruction",
 	/* Author */		"Osku and contributors",
-	/* Version */		'1.0',
+	/* Version */		'1.0.1',
 	/* Permissions */	'admin',
 	/* Priority */		2000
 );
