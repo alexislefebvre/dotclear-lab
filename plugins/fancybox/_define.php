5b36499a5f168d3148b5670c29a1228ae895e5f1
Osku <Osku@b35155ea-a742-4b26-8089-0891bb790021>
2010-07-10 17:07:40 +0000

    fancybox 1.1.2
    git-svn-id: https://svn.dotclear.net/lab@2473 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/fancybox/_define.php
+++ plugins/fancybox/_define.php
@@ -3,7 +3,7 @@
 #
 # This file is part of fancybox, a plugin for Dotclear 2.
 #
-# Copyright (c)  2009 Osku and contributors
+# Copyright (c)  2009 -2010 Osku and contributors
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -14,7 +14,7 @@ $this->registerModule(
 	/* Name */			"FancyBox",
 	/* Description*/		"Fancy image zooming tool",
 	/* Author */			"Osku and contributors",
-	/* Version */			'1.0.1',
+	/* Version */			'1.1.2',
 	/* Permissions */		'admin'
 );
 ?>
--- plugins/fancybox/_public.php
+++ plugins/fancybox/_public.php
@@ -3,7 +3,7 @@
 #
 # This file is part of fancybox, a plugin for Dotclear 2.
 #
-# Copyright (c)  2009 Osku and contributors
+# Copyright (c)  2009 -2010 Osku and contributors
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -22,19 +22,16 @@ class FancyBoxPublic
 		
 		$url = $core->blog->getQmarkURL().'pf='.basename(dirname(__FILE__));
 		echo
-		'<style type="text/css">'."\n".
-		'@import url('.$url.'/css/jquery.fancybox-1.2.5.css);'."\n".
-		"</style>\n".
-		'<script type="text/javascript" src="'.$url.'/js/jquery.fancybox-1.2.5.pack.js"></script>'."\n".
+		'<link rel="stylesheet" href="/?pf=fancybox/css/jquery.fancybox-1.3.1.css" type="text/css" media="screen" />'."\n".
+		'<script type="text/javascript" src="'.$url.'/js/jquery.mousewheel-3.0.2.pack.js"></script>'."\n".
+		'<script type="text/javascript" src="'.$url.'/js/jquery.fancybox-1.3.1.js"></script>'."\n".
 		'<script type="text/javascript">'."\n".
 		"//<![CDATA[\n".
 		'$(function() {'."\n".
 			'$("div.post").each(function() {'."\n".
 					'$(this).find("a[href$=.jpg],a[href$=.jpeg],a[href$=.png],a[href$=.gif],'.
 					'a[href$=.JPG],a[href$=.JPEG],a[href$=.PNG],a[href$=.GIF]").attr("rel","fancy").fancybox({'.
-						'"zoomSpeedIn"		:	500,'.
-						'"zoomSpeedOut"		:	500,'.
-						'"overlayShow"		:	false	'.
+						'"titlePosition" : "over"'.
 					"});\n".
 			"})\n".
 		"});\n".
