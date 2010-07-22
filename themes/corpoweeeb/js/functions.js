0d386484c96a74f16fd7a03b4085b4da718b4f16
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2010-07-22 16:45:19 +0000

    Modif du dock et diverses corrections
    git-svn-id: https://svn.dotclear.net/lab@2502 b35155ea-a742-4b26-8089-0891bb790021

--- themes/corpoweeeb/js/functions.js
+++ themes/corpoweeeb/js/functions.js
@@ -2,12 +2,12 @@ $( function() {
 	$( '#dock li' ).hover( function() {
 		$( '.latest' ).fadeOut( 'fast' );
 		$( this ).addClass( 'dock-active' );
-		$( this ).children( 'span' ).fadeIn( 200 );
-	}).bind( "mouseleave", function() {		
-		$( this ).removeClass( 'dock-active' );	
-		$( this ).children( 'span' ).fadeOut( 200 );
+		$( this ).children( '.display' ).fadeIn( 200 );
+	}).bind( "mouseleave", function() {
+		$( this ).removeClass( 'dock-active' );
+		$( this ).children( '.display' ).fadeOut( 200 );
 	} );
-			
+
 	$( '#dock' ).bind( "mouseleave", function() {
 		$( '.latest' ).fadeIn( 1000 );
 	} );
