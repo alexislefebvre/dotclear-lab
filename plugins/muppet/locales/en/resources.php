5381c1e20f683f6b5a1e82d1f5c58bec43a5a536
Osku <Osku@b35155ea-a742-4b26-8089-0891bb790021>
2010-08-19 17:03:39 +0000

    Oops
    git-svn-id: https://svn.dotclear.net/lab@2558 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/muppet/locales/en/resources.php
@@ -0,0 +1,16 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# This file is part of muppet, a plugin for Dotclear 2.
+#
+# Copyright (c) 2010 Osku and contributors
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+if (!isset($__resources['help']['muppet'])) {
+     $__resources['help']['muppet'] = dirname(__FILE__).'/help.html';
+}
+?>
