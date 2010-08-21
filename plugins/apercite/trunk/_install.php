ec226cdd829dd06ab141edd6bc65d5ab34db9afe
Apercite <Apercite@b35155ea-a742-4b26-8089-0891bb790021>
2010-08-21 10:32:46 +0000

    Merge trunk with branch compatibility-2.2
    git-svn-id: https://svn.dotclear.net/lab@2568 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/apercite/trunk/_install.php
+++ plugins/apercite/trunk/_install.php
@@ -12,26 +12,75 @@
 
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
-$version = $core->plugins->moduleInfo('apercite','version');
-if (version_compare($core->getVersion('apercite'),$version,'>=')) {
-	return;
+$version = $core->plugins->moduleInfo('apercite', 'version');
+if (version_compare($core->getVersion('apercite'), $version, '>='))
+{
+  return;
 }
 
-$settings = new dcSettings($core,$core->blog->id);
-$settings->setNamespace('apercite');
+$core->blog->settings->addNameSpace('apercite');
 
-if(!$settings->get('apercite_enabled')) $settings->put('apercite_enabled',1,'boolean');
-if(!$settings->get('apercite_size')) $settings->put('apercite_size','120x90');
-if(!$settings->get('apercite_javascript')) $settings->put('apercite_javascript',1,'boolean');
-if(!$settings->get('apercite_java')) $settings->put('apercite_java',1,'boolean');
-if(!$settings->get('apercite_login')) $settings->put('apercite_login','');
-if(!$settings->get('apercite_api_key')) $settings->put('apercite_api_key','');
+if (!$core->blog->settings->apercite->get('enabled'))
+{
+  $value = !is_null($core->blog->settings->apercite->get('apercite_enabled')) ? $core->blog->settings->apercite->get('apercite_enabled') : 1;
 
-if(!$settings->get('apercite_local_link')) $settings->put('apercite_local_link',1,'boolean');
+  $core->blog->settings->apercite->put('enabled', $value, 'boolean');
+}
+if (!$core->blog->settings->apercite->get('size'))
+{
+  $value = !is_null($core->blog->settings->apercite->get('apercite_size')) ? $core->blog->settings->apercite->get('apercite_size') : '120x90';
 
+  $core->blog->settings->apercite->put('size', $value);
+}
+if (!$core->blog->settings->apercite->get('javascript'))
+{
+  $value = !is_null($core->blog->settings->apercite->get('apercite_javascript')) ? $core->blog->settings->apercite->get('apercite_javascript') : 1;
 
-if(!$settings->get('apercite_workers')) $settings->put('apercite_workers',serialize(array('.post-excerpt','.post-content')));
+  $core->blog->settings->apercite->put('javascript', $value, 'boolean');
+}
+if (!$core->blog->settings->apercite->get('java'))
+{
+  $value = !is_null($core->blog->settings->apercite->get('apercite_java')) ? $core->blog->settings->apercite->get('apercite_java') : 1;
 
+  $core->blog->settings->apercite->put('java', $value, 'boolean');
+}
+if (!$core->blog->settings->apercite->get('login'))
+{
+  $value = !is_null($core->blog->settings->apercite->get('apercite_login')) ? $core->blog->settings->apercite->get('apercite_login') : '';
 
-$core->setVersion('apercite',$version);
+  $core->blog->settings->apercite->put('login', $value);
+}
+if (!$core->blog->settings->apercite->get('apiKey'))
+{
+  $value = !is_null($core->blog->settings->apercite->get('apercite_api_key')) ? $core->blog->settings->apercite->get('apercite_api_key') : '';
+
+  $core->blog->settings->apercite->put('apiKey', $value);
+}
+
+if (!$core->blog->settings->apercite->get('localLink'))
+{
+  $value = !is_null($core->blog->settings->apercite->get('apercite_local_link')) ? $core->blog->settings->apercite->get('apercite_local_link') : 1;
+
+  $core->blog->settings->apercite->put('localLink', $value, 'boolean');
+}
+
+if (!$core->blog->settings->apercite->get('workers'))
+{
+  $value = !is_null($core->blog->settings->apercite->get('apercite_workers')) ? @unserialize($core->blog->settings->apercite->get('apercite_workers')) : array('.post-excerpt', '.post-content');
+
+  $core->blog->settings->apercite->put('workers', serialize($value));
+}
+
+try
+{
+  $strReq = "DELETE
+    FROM ".$core->prefix."setting
+    WHERE setting_id LIKE 'apercite_%'";
+	$core->con->execute($strReq);
+}
+catch (Exception $e)
+{
+}
+
+$core->setVersion('apercite', $version);
 ?>
