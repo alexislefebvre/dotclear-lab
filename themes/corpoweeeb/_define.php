c3ef4b9b246f5a027a5fd813c582e924645384be
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2010-08-22 15:39:04 +0000

    Ajout _slide-4.html, modif du code des autres slides (div ne servant à rien).
    git-svn-id: https://svn.dotclear.net/lab@2579 b35155ea-a742-4b26-8089-0891bb790021

--- themes/corpoweeeb/_define.php
+++ themes/corpoweeeb/_define.php
@@ -18,6 +18,6 @@ $this->registerModule(
 	/* Description*/		"Thème corporate",
 	/* Author */			"Weeeb (http://www.weeeb.fr) - Pierre Van Glabeke",
 	/* Version */			'1.1'
-	/* date 18082010 */
+	/* date 22082010 */
 );
 ?>
