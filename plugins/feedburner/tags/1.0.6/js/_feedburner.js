605909d19d9fe9fd082935aa079e8a6a54edb72e
Tomtom33 <Tomtom33@b35155ea-a742-4b26-8089-0891bb790021>
2010-09-06 08:20:08 +0000

    tag feedburner 1.0.6
    git-svn-id: https://svn.dotclear.net/lab@2623 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/feedburner/tags/1.0.6/js/_feedburner.js
@@ -0,0 +1,25 @@
+/*
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of feedburner, a plugin for Dotclear.
+# 
+# Copyright (c) 2009-2010 Tomtom
+# http://blog.zenstyle.fr/
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+*/
+
+$(function() {
+	feeds = ['rss2','rss2_comments','atom','atom_comments'];
+	$.each(feeds,function(i) { 
+		if ($("input[name='"+feeds[i]+"']").val().length > 0) {
+			$("input[name='"+feeds[i]+"']").next('p').show("slow");
+		}
+		$("input[name='"+feeds[i]+"']").keyup(function() {
+			if ($(this).val().length > 0) { $(this).next('p').show("slow"); };
+			if ($(this).val().length == 0) { $(this).next('p').hide("slow"); };
+		});
+	});
+});
