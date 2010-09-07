a0d45c91f82ec56fc71f9572f59f8a51b3abfb36
annso <annso@b35155ea-a742-4b26-8089-0891bb790021>
2010-09-07 18:27:57 +0000

    nouveau theme : balcony
    git-svn-id: https://svn.dotclear.net/lab@2625 b35155ea-a742-4b26-8089-0891bb790021

---
+++ themes/meccano/_define.php
@@ -0,0 +1,20 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of Meccano, a theme for Dotclear.
+#
+# Copyright (c) 2009 annso & Lomalarch
+# contact@as-i-am.fr
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			"Meccano",
+	/* Description*/	"Base theme",
+	/* Author */		"annso & Lomalarch",
+	/* Version */		'1.0'
+);
+?>
