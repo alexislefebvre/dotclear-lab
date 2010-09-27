eac04463e0485c33f07d5621467353391367665d
lipki <lipki@b35155ea-a742-4b26-8089-0891bb790021>
2010-09-27 13:45:26 +0000

    insertion automatique de la balise, suite est fin, avec le add qui va bien
    git-svn-id: https://svn.dotclear.net/lab@2679 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/flvplayerconfig/trunk/js/popup.js
@@ -0,0 +1,49 @@
+function urlencode (clearString) {
+  var output = '';
+  var x = 0;
+  if( !clearString ) return '';
+  clearString = clearString.toString();
+  var regex = /(^[a-zA-Z0-9_.]*)/;
+  while (x < clearString.length) {
+    var match = regex.exec(clearString.substr(x));
+    if (match != null && match.length > 1 && match[1] != '') {
+	output += match[1];
+      x += match[1].length;
+    } else {
+      if (clearString[x] == ' ')
+	output += '+';
+      else {
+	var charCode = clearString.charCodeAt(x);
+	var hexVal = charCode.toString(16);
+	output += '%' + ( hexVal.length < 2 ? '0' : '' ) + hexVal.toUpperCase();
+      }
+      x++;
+    }
+  }
+  return output;
+}
+
+$(function(){
+	$('#flvplayerconfig-ok')
+		.click(function(){
+			sendClose();
+			window.close();
+		});
+	
+	function sendClose(){
+		var insert_form=$('#flvplayerconfig-insert-form').get(0);
+		if(insert_form==undefined){return;}
+		var tb=window.opener.the_toolbar;
+		var data=tb.elements.flvplayerconfig.data;
+		
+		ref = {'align':'none', 'loop':'on', 'autoplay':'on', 'autoload':'on', 'volume':'100', 'showmouse':'always', 'videobgcolor':'000000', 'loadonstop':'on', 'phpstream':'on', 'shortcut':'on', 'showtitleandstartimage':'on', 'margin':'5', 'bgcolor':'ffffff', 'bgcolor1':'7c7c7c', 'bgcolor2':'333333', 'showstop':'on', 'showvolume':'on', 'showtime':'0', 'showplayer':'autohide', 'showloading':'autohide', 'showfullscreen':'on', 'showswitchsubtitles':'on', 'playertimeout':'1500', 'playercolor':'000000', 'playeralpha':'100', 'loadingcolor':'ffff00', 'buttoncolor':'ffffff', 'buttonovercolor':'ffff00', 'slidercolor1':'cccccc', 'slidercolor2':'888888', 'sliderovercolor':'ffff00', 'titlecolor':'ffffff', 'titlesize':'20', 'srt':'on', 'srtcolor':'ffffff', 'srtbgcolor':'000000', 'srtsize':'11', 'buffer':'5', 'buffermessage':'Buffering _n_', 'buffercolor':'ffffff', 'bufferbgcolor':'000000', 'buffershowbg':'on', 'onclick':'playpause', 'onclicktarget':'_self', 'ondoubleclick':'none', 'ondoubleclicktarget':'_self', 'showiconplay':'on', 'iconplaycolor':'ffffff', 'iconplaybgcolor':'000000', 'iconplaybgalpha':'75'}
+		
+		$('input').add('select').each(function(index) {
+			if( $(this).attr('id') && $(this).val() != '' )
+				if( $(this).val() != ref[$(this).attr('id')] )
+					data[$(this).attr('id')] = $(this).val();
+		});
+		
+		tb.elements.flvplayerconfig.fncall[tb.mode].call(tb);
+	};
+});
