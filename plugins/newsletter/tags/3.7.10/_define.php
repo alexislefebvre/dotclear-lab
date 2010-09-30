5e453ea0f50e0415dd7d65e72fce00983cc805cb
kwon <kwon@b35155ea-a742-4b26-8089-0891bb790021>
2010-09-30 12:05:39 +0000

    Newsletter 3.7.10 tag
    git-svn-id: https://svn.dotclear.net/lab@2686 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/newsletter/tags/3.7.10/_define.php
@@ -0,0 +1,25 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of Newsletter, a plugin for Dotclear.
+# 
+# Copyright (c) 2009-2010 Benoit de Marne.
+# benoit.de.marne@gmail.com
+# Many thanks to Association Dotclear and special thanks to Olivier Le Bris
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+$this->registerModule(
+	/* Name */			"Newsletter",
+	/* Description*/	"Manage your newsletters in Dotclear 2",
+	/* Author */		"Benoit de Marne",
+	/* Version */		"3.7.10",
+	/* Permissions */	"usage,contentadmin",
+	/* Priority */		null
+);
+
+/* Original Author (3.4.2) : Olivier Le Bris */
+
+?>
