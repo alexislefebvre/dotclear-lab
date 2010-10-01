faab8c37da90ec8a86c62cf73aecd09da6a0de7b
Tomtom33 <Tomtom33@b35155ea-a742-4b26-8089-0891bb790021>
2010-10-01 20:00:35 +0000

    tag myLocation 0.1
    git-svn-id: https://svn.dotclear.net/lab@2694 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/myLocation/tags/0.1/_define.php
@@ -0,0 +1,21 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of myLocation, a plugin for Dotclear.
+#
+# Copyright (c) 2010 Tomtom and contributors
+# http://blog.zenstyle.fr/
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+$this->registerModule(
+		/* Name */			"myLocation",
+		/* Description*/		"Display your current location",
+		/* Author */			"Tomtom (http://blog.zenstyle.fr)",
+		/* Version */			'0.1',
+		/* Permissions */		'usage,contentadmin'
+);
+
+?>
---
+++ plugins/myLocation/tags/0.1/_prepend.php
@@ -0,0 +1,17 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of myLocation, a plugin for Dotclear.
+#
+# Copyright (c) 2010 Tomtom and contributors
+# http://blog.zenstyle.fr/
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
+
+$__autoload['rsExtCommentLocation'] = dirname(__FILE__).'/inc/class.rs.ext.comment.location.php';
+
+?>
