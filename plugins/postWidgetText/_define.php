405b7d1705066e99c9fbe16a1557bb0ac53d47a1
JcDenis <JcDenis@b35155ea-a742-4b26-8089-0891bb790021>
2010-11-12 16:49:00 +0000

    '''postWidgetText 0.6.1:'''  * Fixed install on nightly build
    git-svn-id: https://svn.dotclear.net/lab@2755 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/postWidgetText/_define.php
+++ plugins/postWidgetText/_define.php
@@ -9,4 +9,4 @@
 # A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # -- END LICENSE BLOCK ------------------------------------
-if (!defined('DC_RC_PATH')){return;}$this->registerModule(	/* Name */			"Post widget text",	/* Description*/		"Add a widget with a text related to an entry",	/* Author */		"JC Denis",	/* Version */		'0.6',	/* Permissions */		'usage,contentadmin');	/* date */		#20100608?>
+if (!defined('DC_RC_PATH')){return;}$this->registerModule(	/* Name */			"Post widget text",	/* Description*/		"Add a widget with a text related to an entry",	/* Author */		"JC Denis",	/* Version */		'0.6.1',	/* Permissions */		'usage,contentadmin');	/* date */		#20101112?>
