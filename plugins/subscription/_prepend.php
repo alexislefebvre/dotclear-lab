4709dd45980fd8a685191a031e489a0c0ca519f6
kemar <kemar@b35155ea-a742-4b26-8089-0891bb790021>
2011-01-10 20:28:22 +0000

    (oublié lors du commit précédent)
    git-svn-id: https://svn.dotclear.net/lab@2852 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/subscription/_prepend.php
@@ -0,0 +1,18 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+# This file a plugin of DotClear.
+# Copyright (c) Marc Vachette . All rights
+# reserved.
+#
+#Subscription is free software; you can redistribute it and/or modify
+# it under the terms of the Creative Commons License BY SA
+# see the page http://creativecommons.org/licenses/by-sa/3.0/ for more information
+# 
+# Subscription is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Creative Commons License for more details.
+#
+# ***** END LICENSE BLOCK *****
+
+$__autoload['Subscription']		= dirname(__FILE__).'/inc/class.subscription.php';
