ca2bae0b87cfd5b7f0b30ab02788ded5906f6fbb
JcDenis <JcDenis@b35155ea-a742-4b26-8089-0891bb790021>
2011-01-30 18:14:10 +0000

    minor fix
    git-svn-id: https://svn.dotclear.net/lab@2932 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/dcLibTumblr/_define.php
+++ plugins/dcLibTumblr/_define.php
@@ -16,7 +16,7 @@ $this->registerModule(
 	/* Name */			"Tumblr libraries",
 	/* Description*/		"Tumblr library for Dotclear's plugins",
 	/* Author */			"JC Denis",
-	/* Version */			'0.1-beta1',
+	/* Version */			'0.1-beta2',
 	/* Permissions */		'admin'
 );
 	/* date */		#20110130
