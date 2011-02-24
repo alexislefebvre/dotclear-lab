0ff4317530205ae6a1c0b96f61aa5be97d324444
sacha <sacha@b35155ea-a742-4b26-8089-0891bb790021>
2011-02-24 21:32:47 +0000

    [wiki:plugin/arlequin arlequin] - updated copyright header
    git-svn-id: https://svn.dotclear.net/lab@2992 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/arlequin/_widgets.php
+++ plugins/arlequin/_widgets.php
@@ -1,18 +1,13 @@
-<?php /* -*- tab-width: 5; indent-tabs-mode: t; c-basic-offset: 5 -*- */
-/***************************************************************\
- *  This is 'Arlequin', a plugin for Dotclear 2                *
- *                                                             *
- *  Copyright (c) 2007,2011                                    *
- *  Alex Pirine and contributors.                              *
- *                                                             *
- *  This is an open source software, distributed under the GNU *
- *  General Public License (version 2) terms and  conditions.  *
- *                                                             *
- *  You should have received a copy of the GNU General Public  *
- *  License along with 'Arlequin' (see COPYING.txt);           *
- *  if not, write to the Free Software Foundation, Inc.,       *
- *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    *
-\***************************************************************/
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of Arlequin, a plugin for Dotclear.
+# 
+# Copyright (c) 2007,2008,2011 Alex Pirine <alex pirine.fr>
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
 
 if (!defined('DC_RC_PATH')) { return; }
 
