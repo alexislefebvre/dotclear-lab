820ff99a9764d141b653bc07fb60f5f6cd59259c
dolmen <dolmen@b35155ea-a742-4b26-8089-0891bb790021>
2011-04-10 22:23:23 +0000

    Major new feature: .htaccess generation for old DC1 tags URLs Reworked the page layout. TODO: localization
    git-svn-id: https://svn.dotclear.net/lab@3016 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/dc1redirect/index.php
+++ plugins/dc1redirect/index.php
@@ -3,17 +3,124 @@
 #
 # This file is part of Dotclear 2.
 #
-# Copyright (c) 2006-2008 Pep and contributors. All rights
-# reserved. Licensed under the GPL version 2.0 license.
+# Copyright (c) 2006-2011 Pep, Olivier Mengué and contributors.
+# All rights reserved. Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #
 # -- END LICENSE BLOCK ------------------------------------
 
-preg_match('|^([a-z]{3,}://).*?(/.*)$|',$core->blog->url,$matches);
+
+# WARNING:
+# This file is encoded in iso-8859-15 because it includes some original code from
+# DotClear 1 using that encoding. Switching the encoding to UTF-8 will break
+# the code.
+
+
+# Functions from DotClear 1.x -- BEGIN
+#   inc/classes/class.blog.php
+#
+#   Copyright (c) 2004-2007 Olivier Meunier and contributors. All rights
+#   reserved.
+#
+
+	function decodeUnicodeEntities($str)
+	{
+		return preg_replace_callback('/&#(\\d+);/','code2utf',$str);
+	}
+
+	function code2utf($m)
+	{
+		if ($m[1] < 128) {
+			return chr($m[1]);
+		}
+		if ($m[1] < 2048) {
+			return chr(($m[1] >> 6) + 192).chr(($m[1] & 63) + 128);
+		}
+		if ($m[1] < 65536) {
+			return chr(($m[1] >> 12) + 224).chr((($m[1] >> 6) & 63) + 128).
+			chr(($m[1] & 63) + 128);
+		}
+		if ($m[1] < 2097152) {
+			return chr(($m[1] >> 18) + 240).chr((($m[1] >> 12) & 63) + 28).
+			chr((($m[1] >> 6) & 63) + 128).chr(($m[1] & 63) + 128);
+		}
+		return '';
+	}
+
+	function removeEntities($string)
+	{
+		# Tableau des codes de 130 à 140 et 145 à 156
+		$tags = array('‚' => '&sbquo;','ƒ' => '&fnof;','„' => '&bdquo;',
+		'…' => '&hellip;','†' => '&dagger;','‡' => '&Dagger;','ˆ' => '&circ;',
+		'‰' => '&permil;','Š' => '&Scaron;','‹' => '&lsaquo;','Œ' => '&OElig;',
+		'‘' => '&lsquo;','’' => '&rsquo;','“' => '&ldquo;','”' => '&rdquo;',
+		'•' => '&bull;','–' => '&ndash;','—' => '&mdash;','˜' => '&tilde;',
+		'™' => '&trade;','š' => '&scaron;','›' => '&rsaquo;','œ' => '&oelig;',
+		'Ÿ' => '&Yuml;','€' => '&euro;');
+		
+		$vtags = array(
+		'‚' => '&#8218;','ƒ' => '&#402;','„' => '&#8222;','…' => '&#8230;',
+		'†' => '&#8224;','‡' => '&#8225;','ˆ' => '&#710;','‰' => '&#8240;',
+		'Š' => '&#352;','‹' => '&#8249;','Œ' => '&#338;','‘' => '&#8216;',
+		'’' => '&#8217;','“' => '&#8220;','”' => '&#8221;','•' => '&#8226;',
+		'–' => '&#8211;','—' => '&#8212;','˜' => '&#732;','™' => '&#8482;',
+		'š' => '&#353;','›' => '&#8250;','œ' => '&#339;','Ÿ' => '&#376;',
+		'€' => '&#8364;');
+		
+		if (true /* $dc1_blog_encoding == 'UTF-8' */) {
+			$tags = get_html_translation_table(HTML_ENTITIES);
+			$tags = array_flip($tags);
+			array_walk($tags,create_function('&$v','$v = utf8_encode($v);'));
+			$tags = array_flip($tags);
+			$string = decodeUnicodeEntities($string) ;
+		} else {
+			$tags = array_merge($tags,get_html_translation_table(HTML_ENTITIES));
+		}
+		
+		foreach($tags as $k => $v) {
+			$ASCIItags[$k] = '&#'.ord($k).';';
+		}
+		
+		$string = str_replace($tags,array_flip($tags),$string);
+		$string = str_replace($ASCIItags,array_flip($ASCIItags),$string);
+		$string = str_replace(array_values($vtags),array_keys($vtags),$string);
+		
+		return $string;
+	}
+
+	function str2url($str)
+	{
+		$str = removeEntities(utf8_decode($str));
+	
+		$str = strtr($str,
+		"ÀÁÂÃÄÅàáâãäåÇçÒÓÔÕÖØòóôõöøÈÉÊËèéêëÌÍÎÏìíîïÙÚÛÜùúûü¾ÝÿýÑñ",
+		"AAAAAAaaaaaaCcOOOOOOooooooEEEEeeeeIIIIiiiiUUUUuuuuYYyyNn");
+	
+		$str = str_replace('Æ','AE',$str);
+		$str = str_replace('æ','ae',$str);
+		$str = str_replace('¼','OE',$str);
+		$str = str_replace('½','oe',$str);
+		
+		$str = preg_replace('/[^a-z0-9_\s\'\:\/\[\]-]/','',strtolower($str));
+		
+		$str = preg_replace('/[\s\'\:\/\[\]-]+/',' ',trim($str));
+	
+		$res = str_replace(' ','-',$str);
+		
+		return $res;
+	}
+
+# Functions from DotClear 1.x -- END
+
+
+
+$url = $core->blog->url;
+preg_match('|^([a-z]{3,}://).*?(/.*)$|',$url,$matches);
 $proto = $matches[1];
 $path = $matches[2];
 
+# Template for DC1 feeds PHP script replacement
 $feed_tpl = <<<EOD
 <?php
 //
@@ -41,6 +148,37 @@ if (preg_match('/cgi/',PHP_SAPI)) {
 header('Location: '.\$url); exit;
 ?>
 EOD;
+
+# Prepare tags URL redirections
+$tags = $core->meta->getMetadata(array('type' => 'tag', 'order' => 'meta_id ASC'))->rows();
+
+$tags = array_unique(
+		array_map(
+			create_function('$a', "return \$a['meta_id'];"),
+			$tags),
+		SORT_STRING);
+natcasesort($tags);
+
+$tags_url_base = $core->url->getBase('tag').'/';
+
+$tags_htaccess = array();
+foreach ($tags as $t) {
+	# URL path for DC2 (from the TagURL function in plugins/tags/_public.php)
+	$t_dc2 = $tags_url_base.rawurlencode($t);
+	# URL path for DC1
+	# FIXME handle QueryString configs too
+	$t_dc1 = 'tag/'.str2url($t);
+
+	if ($t_dc2 != $t_dc1) {
+		$tags_htaccess[] = "RedirectPermanent $path$t_dc1 $url$t_dc2";
+	} else {
+		$tags_htaccess[] = "# $t => $t_dc2";
+	}
+}
+
+$tags_htaccess = implode("\n", $tags_htaccess);
+
+
 ?>
 <html>
 <head>
@@ -50,6 +188,12 @@ EOD;
 <body>
 <?php
 echo '<h2>'.__('Dotclear 1 URLs').'</h2>';
+echo
+'<p>'.__('Some redirection can not be directly handled by the "dc1redirect" plugin. '.
+'This page helps you to do the manual modifications required.').'</p>';
+
+
+echo '<h3>'.__('Feeds').'</h3>';
 
 echo
 '<p>'.__('In order to avoid broken feeds, you should replace your old '.
@@ -61,10 +205,22 @@ echo
 'you created them earlier.').'</p>';
 
 echo
-'<h3>atom.php</h3>'.
+'<h4>'.'atom.php</h4>'.
 '<p>'.form::textarea('atom',60,24,html::escapeHTML(sprintf($feed_tpl,'atom')),'maximal').'</p>'.
-'<h3>rss.php</h3>'.
+'<h4>rss.php</h4>'.
 '<p>'.form::textarea('rss2',60,24,html::escapeHTML(sprintf($feed_tpl,'rss2')),'maximal').'</p>';
+
+
+echo '<h3>'.__('Tags').'</h3>';
+
+echo
+'<p>'.__('With DC1 URLs for tags were in lower case, while DC2 now preserves '.
+'case and special characters. You may want to preserve old links to tags by '.
+'adding those lines to .htaccess:').'</p>';
+
+echo
+'<h4>.htaccess</h4>'.
+'<p>'.form::textarea('tags',60,50,html::escapeHTML($tags_htaccess),'maximal').'</p>';
 
 ?>
 </body>
