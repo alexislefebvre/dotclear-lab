d55b70ee10b0fa75a67df004b032df7d5a30715b
JcDenis <JcDenis@b35155ea-a742-4b26-8089-0891bb790021>
2011-06-10 15:34:19 +0000

    '''postWidgetText 2011.06.10'''  * Fixed bug on export func (thks @noe)  * (c) 2011
    git-svn-id: https://svn.dotclear.net/lab@3056 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/postWidgetText/_public.php
+++ plugins/postWidgetText/_public.php
@@ -2,13 +2,14 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of postWidgetText, a plugin for Dotclear 2.
 # 
-# Copyright (c) 2009-2010 JC Denis and contributors
+# Copyright (c) 2009-2011 JC Denis and contributors
 # jcdenis@gdwd.com
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # -- END LICENSE BLOCK ------------------------------------
-if (!defined('DC_RC_PATH')){return;}
+
+if (!defined('DC_RC_PATH')){return;}
 
 $core->blog->settings->addNamespace('postwidgettext'); require dirname(__FILE__).'/_widgets.php';class postWidgetTextPublicBehaviors{	public static function widget($w)	{		global $core, $_ctx; 				if (!$core->blog->settings->postwidgettext->postwidgettext_active		 || !$_ctx->exists('posts') 		 || !$_ctx->posts->post_id) return;				$title = (strlen($w->title) > 0) ? 				'<h2>'.html::escapeHTML($w->title).'</h2>' : null;		$content = '';				$pwt = new postWidgetText($core);		$rs = $pwt->getWidgets(array('post_id'=>$_ctx->posts->post_id));				if ('' != $rs->option_title)		{			$title = '<h2>'.$rs->option_title.'</h2>';		}		if ('' != $rs->option_content_xhtml)		{			$content = $rs->option_content_xhtml;		}		if ('' == $content && $w->excerpt)		{			$content = $_ctx->posts->post_excerpt_xhtml;		}		if ('' == $content && !$w->show)		{			return;		}		return '<div class="postwidgettext">'.$title.$content.'</div>';	}}?>
--- plugins/postWidgetText/_widgets.php
+++ plugins/postWidgetText/_widgets.php
@@ -2,11 +2,12 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of postWidgetText, a plugin for Dotclear 2.
 # 
-# Copyright (c) 2009-2010 JC Denis and contributors
+# Copyright (c) 2009-2011 JC Denis and contributors
 # jcdenis@gdwd.com
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # -- END LICENSE BLOCK ------------------------------------
-if (!defined('DC_RC_PATH')){return;}$core->addBehavior('initWidgets',	array('postWidgetTextWidgetBehaviors','init'));class postWidgetTextWidgetBehaviors{	public static function init($w)	{		global $core;		$w->create('postwidgettext',__('Post widget text'),			array('postWidgetTextPublicBehaviors','widget'));		$w->postwidgettext->setting('title',__('Title:'),			__('More about this entry'),'text');		$w->postwidgettext->setting('excerpt',__('Use excerpt if no content'),			0,'check');		$w->postwidgettext->setting('show',__('Show widget even if empty'),			0,'check');	}}?>
+
+if (!defined('DC_RC_PATH')){return;}$core->addBehavior('initWidgets',	array('postWidgetTextWidgetBehaviors','init'));class postWidgetTextWidgetBehaviors{	public static function init($w)	{		global $core;		$w->create('postwidgettext',__('Post widget text'),			array('postWidgetTextPublicBehaviors','widget'));		$w->postwidgettext->setting('title',__('Title:'),			__('More about this entry'),'text');		$w->postwidgettext->setting('excerpt',__('Use excerpt if no content'),			0,'check');		$w->postwidgettext->setting('show',__('Show widget even if empty'),			0,'check');	}}?>
