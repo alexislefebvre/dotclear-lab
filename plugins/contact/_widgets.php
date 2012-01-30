08e26bbaa55fbb3f7579e13b0d8e5613eeb24c01
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2012-01-30 14:52:59 +0000

    licence
    git-svn-id: https://svn.dotclear.net/lab@3096 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/contact/_widgets.php
+++ plugins/contact/_widgets.php
@@ -1,23 +1,14 @@
 <?php
-# ***** BEGIN LICENSE BLOCK *****
-# This is Contact, a plugin for DotClear.
-# Copyright (c) 2005 k-net. All rights reserved.
-#
-# DotClear is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# DotClear is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with DotClear; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# ***** END LICENSE BLOCK *****
+/* BEGIN LICENSE BLOCK
+This file is part of Contact, a plugin for Dotclear.
+
+K-net
+Pierre Van Glabeke
+
+Licensed under the GPL version 2.0 license.
+A copy of this license is available in LICENSE file or at
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+END LICENSE BLOCK */
 if (!defined('DC_RC_PATH')) { return; }
 
 $core->addBehavior('initWidgets',array('contactWidget','initWidgets'));
