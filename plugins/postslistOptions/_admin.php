d1c9644e860286de972b5fb7383bc073807526e3
JcDenis <JcDenis@b35155ea-a742-4b26-8089-0891bb790021>
2013-07-10 07:01:49 +0000

    2013.07.10  * Fixed action on multiple posts
    git-svn-id: https://svn.dotclear.net/lab@3140 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/postslistOptions/_admin.php
+++ plugins/postslistOptions/_admin.php
@@ -1,22 +1,24 @@
 <?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
+#
 # This file is part of postslistOptions, a plugin for Dotclear 2.
 # 
-# Copyright (c) 2009-2010 JC Denis and contributors
-# jcdenis@gdwd.com
+# Copyright (c) 2009-2013 Jean-Christian Denis and contributors
+# contact@jcdenis.fr http://jcd.lv
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
 # -- END LICENSE BLOCK ------------------------------------
 
 if (!defined('DC_CONTEXT_ADMIN')){return;}
 
-if ($core->auth->check('admin',$core->blog->id)) {
-	$core->addBehavior('adminPostsActionsCombo',array('behaviorsPostlistOptions','adminPostsActionsCombo'));
-	$core->addBehavior('adminPostsActionsContent',array('behaviorsPostlistOptions','adminPostsActionsContent'));
-	$core->addBehavior('adminPostsActions',array('behaviorsPostlistOptions','adminPostsActions'));
-}
+if (!$core->auth->check('admin',$core->blog->id)) { return; }
+
+$core->addBehavior('adminPostsActionsCombo',array('behaviorsPostlistOptions','adminPostsActionsCombo'));
+$core->addBehavior('adminPostsActionsContent',array('behaviorsPostlistOptions','adminPostsActionsContent'));
+$core->addBehavior('adminPostsActions',array('behaviorsPostlistOptions','adminPostsActions'));
 
 class behaviorsPostlistOptions
 {
@@ -93,9 +95,9 @@ class behaviorsPostlistOptions
 					self::updPostOption($id,'nb_trackback',0);
 					break;
 				}
-				if (!$core->error->flag()) {
-					http::redirect($redir);
-				}
+			}
+			if (!$core->error->flag()) {
+				http::redirect($redir);
 			}
 		}
 		catch (Exception $e)
