01fc3f1a666f02fd78a81338bbe29c4a43329316
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2013-08-01 09:34:50 +0000

    upload de la v1.3 présente sur DA
    git-svn-id: https://svn.dotclear.net/lab@3148 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/contactMe/_public.php
+++ plugins/contactMe/_public.php
@@ -276,7 +276,8 @@ class tplContactMe
 	{
 		global $core;
 		
-		if ($w->homeonly && $core->url->type != 'default') {
+		if (($w->homeonly == 1 && $core->url->type != 'default') ||
+			($w->homeonly == 2 && $core->url->type == 'default')) {
 			return;
 		}
 		
@@ -285,12 +286,12 @@ class tplContactMe
 		}
 		
 		$res =
-		'<div class="contact-me">'.
+		($w->content_only ? '' : '<div class="contact-me'.($w->class ? ' '.html::escapeHTML($w->class) : '').'">').
 		($w->title ? '<h2>'.html::escapeHTML($w->title).'</h2>' : '').
 		'<p><a href="'.$core->blog->url.$core->url->getBase('contactme').'">'.
 		($w->link_title ? html::escapeHTML($w->link_title) : __('Contact me')).
 		'</a></p>'.
-		'</div>';
+		($w->content_only ? '' : '</div>');
 		
 		return $res;
 	}
