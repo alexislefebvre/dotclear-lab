f65e6b78f10b729561c79b487fc73cbeb9b85a61
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2013-08-01 10:37:06 +0000

    version 0.8.1-dsls (cf quickfix : http://forum.dotclear.org/viewtopic.php?pid=286550#p286550)
    git-svn-id: https://svn.dotclear.net/lab@3151 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/dcom/_define.php
+++ plugins/dcom/_define.php
@@ -1,21 +1,25 @@
-<?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of Dcom, a plugin for Dotclear.
-# 
-# Copyright (c) 2007,2008,2011 Alex Pirine <alex pirine.fr>
-# 
-# Licensed under the GPL version 2.0 license.
-# A copy is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
-
+<?php /* -*- tab-width: 5; indent-tabs-mode: t; c-basic-offset: 5 -*- */
+/***************************************************************\
+ *  This is 'Dcom', a plugin for Dotclear 2                    *
+ *                                                             *
+ *  Copyright (c) 2007-2008                                    *
+ *  Oleksandr Syenchuk, Jean-François Michaud and contributors.*
+ *                                                             *
+ *  This is an open source software, distributed under the GNU *
+ *  General Public License (version 2) terms and  conditions.  *
+ *                                                             *
+ *  You should have received a copy of the GNU General Public  *
+ *  License along with 'Dcom' (see COPYING.txt);               *
+ *  if not, write to the Free Software Foundation, Inc.,       *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    *
+\***************************************************************/
 if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
 	/* Name */		'Dcom',
 	/* Description*/	'Last comments with more options',
-	/* Author */		'Alex Pirine',
-	/* Version */		'2011.02',
+	/* Author */		'Oleksandr Syenchuk',
+	/* Version */		'0.8.1-dsls',
 	/* Permissions */	'admin'
 );
 ?>
