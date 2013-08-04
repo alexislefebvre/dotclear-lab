07b53423abff593f36ce405b0e82a77faa1207f0
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2013-08-04 18:48:26 +0000

    version 1.3
    git-svn-id: https://svn.dotclear.net/lab@3167 b35155ea-a742-4b26-8089-0891bb790021

---
+++ themes/dc2smartphone/_define.php
@@ -0,0 +1,20 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of dc2smartphone, a theme for Dotclear.
+# Copyright (c) 2009
+# Guillaume Bizet guillaumebizet@gmail.com
+# Pierre Van Glabeke contact@brol.info
+#
+# Licensed under the Creative Commons license.
+# A copy of this license is available at
+# http://creativecommons.org/licenses/by-nc-sa/2.0/fr/
+# -- END LICENSE BLOCK ------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			"dc2smartphone",
+	/* Description*/		"Thème pour dc2 optimisé pour les smartphones",
+	/* Author */			"Guillaume BIZET | http://www.guillaumebizet.fr, Pierre Van Glabeke",
+	/* Version */			'1.3'
+);
+?>
