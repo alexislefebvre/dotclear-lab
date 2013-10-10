c2e9fda322dbb5872b1d6d7f6f5a5ce76a6a2b4f
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2013-10-10 21:53:43 +0000

    version 1.1 : màj css + html, compatibilité dc 2.6
    git-svn-id: https://svn.dotclear.net/lab@3212 b35155ea-a742-4b26-8089-0891bb790021

--- themes/sterenn/_define.php
+++ themes/sterenn/_define.php
@@ -1,20 +1,18 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
+# ***** BEGIN LICENSE BLOCK *****
 #
-# This file is part of Dotclear 2.
+# Sterenn
+# Theme by Ludovic Toinel
+# Contributor : Pierre Van Glabeke
+# License: public domain
 #
-# Copyright (c) 2003-2008 Olivier Meunier and contributors
-# Licensed under the GPL version 2.0 license.
-# See LICENSE file or
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
-# -- END LICENSE BLOCK ------------------------------------
+# ***** END LICENSE BLOCK *****
 if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
-	/* Name */				"Sterenn",
+	/* Name */				  "Sterenn",
 	/* Description*/		"Le thème d'une étoile bretonne",
-	/* Author */			"Ludovic Toinel",
-	/* Version */			'1.1'
+	/* Author */			  "Ludovic Toinel",
+	/* Version */			  '1.1'
 );
 ?>
