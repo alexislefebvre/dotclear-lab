f061980d84ad8544c3251c47ee8877cd5d6ef801
JcDenis <JcDenis@b35155ea-a742-4b26-8089-0891bb790021>
2013-10-28 16:42:01 +0000

    Update french help and support links
    git-svn-id: https://svn.dotclear.net/lab@3243 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/pacKman/locales/fr/resources.php
+++ plugins/pacKman/locales/fr/resources.php
@@ -1,16 +1,22 @@
 <?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
+#
 # This file is part of pacKman, a plugin for Dotclear 2.
 # 
-# Copyright (c) 2009 JC Denis and contributors
-# jcdenis@gdwd.com
+# Copyright (c) 2009-2013 Jean-Christian Denis and contributors
+# contact@jcdenis.fr
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
 # -- END LICENSE BLOCK ------------------------------------
 
-if (!defined('DC_RC_PATH')){return;}
+if (!defined('DC_RC_PATH')) {
 
-$__resources['help']['pacKman'] = dirname(__FILE__).'/help/help.html';
-?>
+	return null;
+}
+
+if (!empty($_REQUEST['module']) && $_REQUEST['module'] == 'pacKman') {
+	$__resources['help']['core_plugins_conf'] = dirname(__FILE__).'/help/help.html';
+}
