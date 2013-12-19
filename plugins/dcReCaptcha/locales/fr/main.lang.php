a7eb488425d3c8222748d8f2b131d2894f7181aa
kwon <kwon@b35155ea-a742-4b26-8089-0891bb790021>
2013-12-19 20:15:21 +0000

    dcReCaptcha 0.0.1
    git-svn-id: https://svn.dotclear.net/lab@3282 b35155ea-a742-4b26-8089-0891bb790021

---
+++ plugins/dcReCaptcha/locales/fr/main.lang.php
@@ -0,0 +1,15 @@
+<?php
+$GLOBALS['__l10n']['delete all settings'] = 'supprime tous les paramètres';
+$GLOBALS['__l10n']['reCaptcha spam filter'] = 'Filtre d\'indésirable reCaptcha';
+$GLOBALS['__l10n']['red (Default theme)'] = 'red (thème par défaut)';
+$GLOBALS['__l10n']['white'] = 'white';
+$GLOBALS['__l10n']['blackglass'] = 'blackglass';
+$GLOBALS['__l10n']['clean'] = 'clean';
+$GLOBALS['__l10n']['reCaptcha settings'] = 'Paramètres de reCaptcha';
+$GLOBALS['__l10n']['Get your owns API key'] = 'Obtenir vos propres clés pour l\'API';
+$GLOBALS['__l10n']['Public Key'] = 'Public key';
+$GLOBALS['__l10n']['Private key'] = 'Private key';
+$GLOBALS['__l10n']['Theme'] = 'Thème';
+$GLOBALS['__l10n']['Enable for comments form'] = 'Activer pour les commentaires des billets';
+$GLOBALS['__l10n']['reCAPTCHA is a free CAPTCHA service that protects your site against spam.'] = 'reCAPTCHA est un service gratuit de CAPTCHA qui protège votre site contre le spam.';
+?>
