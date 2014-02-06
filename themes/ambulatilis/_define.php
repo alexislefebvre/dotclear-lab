2bf10c252b035efb28117abc519b637e38ac4442
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2014-02-06 23:03:48 +0000

    v1.2 : compatibilité dc2.6, php5.3, corrections de bugs, mise à jour jquery, ajouts tpl.
    git-svn-id: https://svn.dotclear.net/lab@3290 b35155ea-a742-4b26-8089-0891bb790021

--- themes/ambulatilis/_define.php
+++ themes/ambulatilis/_define.php
@@ -4,6 +4,7 @@
 # This file is part of ambulatis, a theme for Dotclear.
 #
 # 2012 - annso - contact@as-i-am.fr
+# Contributor : Pierre Van Glabeke
 #
 # Copyright (c) 2003-2010 Olivier Meunier & Association Dotclear
 # Licensed under the GPL version 2.0 license.
@@ -16,7 +17,9 @@ if (!defined('DC_RC_PATH')) { return; }
 $this->registerModule(
 	/* Name */			"Ambulatilis",
 	/* Description*/	"A mobile theme",
-	/* Author */		"annso",
-	/* Version */		'1.1'
+	/* Author */		"annso, Pierre Van Glabeke",
+	/* Version */		'1.2',
+	array(
+		'type'		=>		'theme'
+	)
 );
-?>
