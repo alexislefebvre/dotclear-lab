778ab1126af36ce118eb27abd15d22a8ffddf3d6
vdanjean <vdanjean@b35155ea-a742-4b26-8089-0891bb790021>
2014-03-21 10:27:45 +0000

    Add myself as author
    git-svn-id: https://svn.dotclear.net/lab@3300 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/emailNotification/_define.php
+++ plugins/emailNotification/_define.php
@@ -4,6 +4,7 @@
 # This file is part of Dotclear 2.
 #
 # Copyright (c) 2003-2008 Olivier Meunier and contributors
+#               2014 Vincent Danjean
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -14,7 +15,7 @@ if (!defined('DC_RC_PATH')) { return; }
 $this->registerModule(
 	/* Name */			"Email notification",
 	/* Description*/		"Email notification",
-	/* Author */			"Olivier Meunier",
+	/* Author */			"Olivier Meunier, Vincent Danjean",
 	/* Version */			'1.1.0',
 	/* Properties */
 	array(
--- plugins/emailNotification/behaviors.php
+++ plugins/emailNotification/behaviors.php
@@ -4,6 +4,7 @@
 # This file is part of Dotclear 2.
 #
 # Copyright (c) 2003-2008 Olivier Meunier and contributors
+#               2014 Vincent Danjean
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
