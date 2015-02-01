f7208b71c37b005126c5c47a5994369f7f5dcd28
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2015-02-01 16:55:54 +0000

    v1.1 voir changelog + suppression fichiers non utilisés
    git-svn-id: https://svn.dotclear.net/lab@3343 b35155ea-a742-4b26-8089-0891bb790021

---
+++ themes/balcony/_public.php
@@ -0,0 +1,29 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of Balcony, a theme for Dotclear.
+#
+# Copyright (c) 2010
+# annso contact@as-i-am.fr
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
+
+l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/public');
+
+# appel css menu
+$core->addBehavior('publicHeadContent','balconymenu_publicHeadContent');
+
+function balconymenu_publicHeadContent($core)
+{
+	$style = $core->blog->settings->themes->balcony_menu;
+	if (!preg_match('/^simplemenu|menu-no$/',$style)) {
+		$style = 'simplemenu';
+	}
+
+	$url = $core->blog->settings->themes_url.'/'.$core->blog->settings->theme;
+	echo '<link rel="stylesheet" type="text/css" media="projection, screen" href="'.$url."/".$style.".css\" />\n";
+}
