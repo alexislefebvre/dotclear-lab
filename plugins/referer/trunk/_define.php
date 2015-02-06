2fe9adced7cbb42abc9fea204e41e4127a05b33d
brol <brol@b35155ea-a742-4b26-8089-0891bb790021>
2015-02-06 18:49:10 +0000

    voir changelog
    git-svn-id: https://svn.dotclear.net/lab@3356 b35155ea-a742-4b26-8089-0891bb790021

--- plugins/referer/trunk/_define.php
+++ plugins/referer/trunk/_define.php
@@ -2,8 +2,7 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of referer, a plugin for Dotclear.
 # 
-# Copyright (c) 2008-2010 Tomtom
-# http://blog.zenstyle.fr/
+# Copyright (c) 2008-2015 Tomtom and contributors
 # 
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
@@ -15,8 +14,8 @@ if (!defined('DC_RC_PATH')) { return; }
 $this->registerModule(
 		/* Name */			'referer',
 		/* Description */		'Displays your blog referers',
-		/* Author */			'Tomtom (http://plugins.zenstyle.fr/), Pierre Van Glabeke',
-		/* Version */			'0.4',
+		/* Author */			'Tomtom, Pierre Van Glabeke',
+		/* Version */			'0.5',
 	/* Properties */
 	array(
 		'permissions' => 'usage,contentadmin',
